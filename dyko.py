bf1249124687e0cd9801e11089a2bf33afd4438e
Simon Sapin <simon.sapin@kozea.fr>
2009-06-16 14:47:08 +0200

    Test runner

--- dyko.py
+++ dyko.py
@@ -6,6 +6,6 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 
 
 if __name__ == '__main__':
-    from kraken import script
-    script.run()
+    from kraken import script, site
+    script.run(site.Site())
 
