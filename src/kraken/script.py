0a637e8e9b034e7ffd10b87fe86f2d763075bafb
Simon Sapin <simon.sapin@kozea.fr>
2009-06-16 16:13:13 +0200

    Limit tests to kalamar, kraken, koral

--- src/kraken/script.py
+++ src/kraken/script.py
@@ -18,26 +18,30 @@
 # along with Koral library.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import with_statement
+from itertools import chain, imap
 import os
 import doctest
 from werkzeug import script, import_string
 
 SRC_DIR = os.path.dirname(os.path.dirname(__file__))
 
-def run_tests(base=SRC_DIR, module=()):
+def list_modules(module, base=SRC_DIR):
     dirname = os.path.join(base, *module)
     for basename in os.listdir(dirname):
         path = os.path.join(dirname, basename)
-        if os.path.isdir(path) and \
-           os.path.isfile(os.path.join(path, '__init__.py')):
-            submodule = module + (basename,)
-            path = os.path.join(path, '__init__.py')
-            run_tests(base, submodule)
-        elif basename.endswith('.py') and basename != '__init__.py':
-            submodule = module + (basename[:-3],)            
-        else:
-            continue
-        doctest.testmod(import_string('.'.join(submodule)))
+        if os.path.isdir(path):
+            for i in list_modules(module + (basename,), base):
+                yield i
+        elif basename.endswith('.py'):
+            if basename == '__init__.py':
+                yield module, path
+            else:
+                yield module + (basename[:-3],), path
+
+def run_tests():
+    for name, path in chain(*imap(list_modules, 
+                                  (('kalamar',), ('kraken',), ('koral',)))):
+        doctest.testmod(import_string('.'.join(name)))
         with open(path) as f:
             todo = f.read().count('TODO')
         if todo:
@@ -49,7 +53,7 @@ def run(site):
     action_runserver = script.make_runserver(site, use_reloader=True,
                                              use_debugger=True)
     action_shell = script.make_shell(lambda: {'site': site})
-    action_test = lambda: run_tests()
+    action_test = run_tests
     script.run()
 
 if __name__ == '__main__':
