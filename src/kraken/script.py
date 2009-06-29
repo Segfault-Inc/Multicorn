f6f9f72b54f5a059e1a236f066168235e31a8caf
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2009-06-29 14:17:02 +0200

    Fix header text in all python files

--- src/kraken/script.py
+++ src/kraken/script.py
@@ -15,7 +15,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Koral library.  If not, see <http://www.gnu.org/licenses/>.
+# along with Kraken.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import with_statement
 from itertools import chain, imap
