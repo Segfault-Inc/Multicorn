01c81363b714e28a38c9cabf740499354fee1793
Simon Sapin <simon.sapin@kozea.fr>
2009-06-30 18:32:48 +0200

    Don’t fail miserably if genshi isn’t installed. (Issue a warning instead)

--- koral/engine/genshi_.py
+++ koral/engine/genshi_.py
@@ -15,36 +15,43 @@
 # You should have received a copy of the GNU General Public License
 # along with Koral library.  If not, see <http://www.gnu.org/licenses/>.
 
+import warnings
+
 from koral.engine.base import BaseEngine
 
-class GenshiEngine(BaseEngine):
-    """
-    Koral engine for Genshi: http://genshi.edgewall.org/
-    
-    >>> import koral.site, test.koral, os.path
-    >>> path = os.path.join(os.path.dirname(test.koral.__file__), 'templates')
-    >>> engine = koral.site.Site(path).get_engine('genshi')
-    >>> print engine.render('hello.genshi.html')
-    ...   # doctest: +NORMALIZE_WHITESPACE
-    <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
-                          "http://www.w3.org/TR/html4/strict.dtd">
-    <html><body>Hello, World!</body></html>
-    >>> print engine.render('hello.genshi.html', {'name': 'Python'})
-    ...   # doctest: +NORMALIZE_WHITESPACE
-    <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
-                          "http://www.w3.org/TR/html4/strict.dtd">
-    <html><body>Hello, Python!</body></html>
-    """
-    
-    name = 'genshi'
-    
-    def __init__(self, *args, **kwargs):
-        super(GenshiEngine, self).__init__(*args, **kwargs)
-        from genshi.template import TemplateLoader
-        self._loader = TemplateLoader(self.path_to_root, auto_reload=True)
+try:
+    from genshi.template import TemplateLoader
+except ImportError:
+    warnings.warn('Can not import genshi. '
+                  'GenshiEngine will not be available.')
+else:    
+    class GenshiEngine(BaseEngine):
+        """
+        Koral engine for Genshi: http://genshi.edgewall.org/
         
-    def render(self, template_name, values={}, lang=None, modifiers=None):
-        template = self._loader.load(template_name)
-        return template.generate(**values).render('html', doctype='html')
+        >>> import koral.site, test.koral, os.path
+        >>> path = os.path.join(os.path.dirname(test.koral.__file__), 'templates')
+        >>> engine = koral.site.Site(path).get_engine('genshi')
+        >>> print engine.render('hello.genshi.html')
+        ...   # doctest: +NORMALIZE_WHITESPACE
+        <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+                              "http://www.w3.org/TR/html4/strict.dtd">
+        <html><body>Hello, World!</body></html>
+        >>> print engine.render('hello.genshi.html', {'name': 'Python'})
+        ...   # doctest: +NORMALIZE_WHITESPACE
+        <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+                              "http://www.w3.org/TR/html4/strict.dtd">
+        <html><body>Hello, Python!</body></html>
+        """
+        
+        name = 'genshi'
+        
+        def __init__(self, *args, **kwargs):
+            super(GenshiEngine, self).__init__(*args, **kwargs)
+            self._loader = TemplateLoader(self.path_to_root, auto_reload=True)
+            
+        def render(self, template_name, values={}, lang=None, modifiers=None):
+            template = self._loader.load(template_name)
+            return template.generate(**values).render('html', doctype='html')
 
 
