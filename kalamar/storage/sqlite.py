e0901904362814acbc4aa9e885db9df89c34bbe8
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2009-07-01 10:44:39 +0200

    Kalamar SQLite storage cleanup

--- kalamar/storage/sqlite.py
+++ kalamar/storage/sqlite.py
@@ -15,18 +15,26 @@
 # You should have received a copy of the GNU General Public License
 # along with Kalamar.  If not, see <http://www.gnu.org/licenses/>.
 
-from dbapi import DBAPIStorage
+"""
+SQLite 3 access point.
+
+This implementation depends on DBAPIStorage, the generic database acces point.
+
+"""
+
 import sqlite3
 import urlparse
 
+from dbapi import DBAPIStorage
+
 class SQLiteStorage(DBAPIStorage):
-    
+    """SQLite 3 access point"""
     protocol = 'sqlite'
     
     def get_connection(self):
         """Return (connection, table)
         
-        Needs 'url' in the configuration in the following format:
+        Need 'url' in the configuration in the following format:
             sqlite:///path/to/file?table
         or:
             sqlite://./rel/path/to/file?table
@@ -34,23 +42,24 @@ class SQLiteStorage(DBAPIStorage):
             sqlite://:memory:?table
         
         """
-        if getattr(self, '_connection', None) is None:
+        if not getattr(self, '_connection', None):
             url = self.config['url']
-            urldict = urlparse.urlsplit(url)
-            splitted_path = urldict.path.split('?',1)
+            url_dict = urlparse.urlsplit(url)
+            splitted_path = url_dict.path.split('?', 1)
             file = splitted_path[0][2:]
             self._table = splitted_path[1]
             self._connection = sqlite3.connect(file)
+
         return (self._connection, self._table)
     
     def _get_primary_keys(self):
-        """Return the list of the table's primary keys' names.
+        """Return the list of the table primary keys.
         
         Fixture
         >>> from kalamar._test import fill_sqlite_db
         >>> storage = SQLiteStorage(url='sqlite://:memory:?test', basedir='')
-        >>> conn, table = storage.get_connection()
-        >>> fill_sqlite_db(conn)
+        >>> connection, table = storage.get_connection()
+        >>> fill_sqlite_db(connection)
         
         Test
         >>> storage._get_primary_keys()
@@ -61,9 +70,8 @@ class SQLiteStorage(DBAPIStorage):
         cursor = connection.cursor()
         cursor.execute('PRAGMA table_info(%s)' % table)
         
-        # cid, name, type, notnull, dflt_value, pk
         fields = (dict(zip(['cid', 'name', 'type', 'notnull',
                             'dflt_value', 'pk'], values))
                   for values in cursor.fetchall())
-        pkeys = [f['name'] for f in fields if f['pk']]
-        return pkeys
+
+        return [field['name'] for field in fields if field['pk']]
