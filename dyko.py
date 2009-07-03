5715c8d0925727a43d30c9d5f6422e42a96eebbf
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2009-07-03 09:45:13 +0200

    Dyko test script cleanup

--- dyko.py
+++ dyko.py
@@ -1,30 +1,25 @@
 #!/usr/bin/env python
 
 import sys, os
-
 sys.path.insert(0, os.path.dirname(__file__))
-
 import werkzeug.script
+
 import kraken
 import test
 
 action_runserver = werkzeug.script.make_runserver(
-    kraken.site.Site, use_reloader=True, use_debugger=True
-)
+    kraken.site.Site, use_reloader=True, use_debugger=True)
                                          
-def action_test(packages='kalamar,koral,test', coverage=False, todo=False):
-    """
-    Run all doctests and unittests found in ``packages``
-    """
-    packages = packages.split(',')
+def action_test(packages='kalamar, koral, test', coverage=False, todo=False):
+    """Run all doctests and unittests found in "packages"."""
+    packages = packages.replace(' ', '').split(',')
     if coverage:
         test.run_tests_with_coverage(packages)
     else:
         test.run_tests(packages)
     if todo:
         for module, todos in test.find_TODOs(packages):
-            print module, ':', todos, 'TO' 'DO'+('s' if todos>1 else '')
+            print module, ':', todos, 'TO' 'DO' + ('s' * (todos>1))
 
 if __name__ == '__main__':
     werkzeug.script.run()
-
