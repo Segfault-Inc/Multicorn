dbaaa3739c0f3575e47e158df4d1fa54184cf0d5
Simon Sapin <simon.sapin@kozea.fr>
2009-07-06 18:26:22 +0200

    Add kraken.Site.find_template with tests and test data

--- test/kraken/__init__.py
+++ test/kraken/__init__.py
@@ -8,7 +8,7 @@ def make_site():
     # import kraken here so that coverage sees module-level statements
     import kraken
     return kraken.Site(
-        site_root=os.path.join(os.path.dirname(__file__), 'data'),
+        site_root=os.path.join(os.path.dirname(__file__), 'site'),
         kalamar_conf=os.path.join(os.path.dirname(__file__), '..',
                                   'kalamar', 'data', 'kalamar.conf'),
     )
@@ -27,5 +27,5 @@ class TestHello(KrakenSiteMixin, TestCase):
     def test_hello(self):
         resp = self.client.get('/hello')
         self.assertEqual(resp.status_code, 200)
-        self.assertEqual(resp.data, 'Hello, World!')
+        self.assert_('Hello, World!' in resp.data)
 
