b991f62797105292560ea2bb29a58ca10ec9a8ed
Simon Sapin <simon.sapin@exyr.org>
2009-07-15 18:20:45 +0200

    Move kraken tests

--- test/kraken/__init__.py
+++ test/kraken/__init__.py
@@ -1,6 +1,5 @@
 
 import os.path
-from unittest import TestCase
 import werkzeug
 
 
@@ -23,77 +22,4 @@ class KrakenSiteMixin(object):
         self.test_app = make_site()
         
         self.client = werkzeug.Client(self.test_app, werkzeug.BaseResponse)
-
-class TestRequests(KrakenSiteMixin, TestCase):
-    def test_notfound(self):
-        r = self.client.get('/nonexistent')
-        self.assertEqual(r.status_code, 404)
-
-    def test_static_notfound(self):
-        r = self.client.get('/__logo/inexistent.png')
-        self.assertEqual(r.status_code, 404)
-
-    def test_lipsum_notfound(self):
-        r = self.client.get('/lorem/ipsum/dolor')
-        self.assertEqual(r.status_code, 404)
-
-    def test_index(self):
-        r = self.client.get('/')
-        self.assertEqual(r.status_code, 200)
-        self.assertEqual(r.headers['Content-Type'], 'text/html; charset=utf-8')
-        self.assertEqual(r.data, '<!DOCTYPE html PUBLIC '
-                         '"-//W3C//DTD HTML 4.01//EN" '
-                         '"http://www.w3.org/TR/html4/strict.dtd">\n'
-                         '<html><body>Dyko root</body></html>')
-
-    def test_hello(self):
-        r = self.client.get('/hello')
-        self.assertEqual(r.status_code, 200)
-        self.assertEqual(r.headers['Content-Type'], 'text/html; charset=utf-8')
-        self.assertEqual(r.data, '<html><body>Hello, World!</body></html>')
-
-    def test_lipsum(self):
-        r = self.client.get('/lorem/ipsum')
-        self.assertEqual(r.status_code, 200)
-        self.assertEqual(r.headers['Content-Type'], 'text/plain; charset=utf-8')
-        self.assert_('Lorem ipsum dolor sit amet' in r.data)
-
-    def test_logo(self):
-        r = self.client.get('/__logo/dyko.png')
-        self.assertEqual(r.status_code, 200)
-        self.assertEqual(r.headers['Content-Type'], 'image/png')
-        # Maybe check the actual content here instead of just the length ?
-        self.assertEqual(len(r.data), 12677)
-
-    def test_logo_etag(self):
-        # assume that self.test_logo() passed
-        initial_response = self.client.get('/__logo/dyko.png')
-        
-        response = self.client.get('/__logo/dyko.png', headers=[
-            ('If-None-Match', initial_response.headers['ETag']),
-        ])
-        self.assertEqual(response.status_code, 304)
-        self.assertEqual(response.data, '')
-
-    def test_logo_last_modified(self):
-        # assume that self.test_logo() passed
-        initial_response = self.client.get('/__logo/dyko.png')
-        
-        response = self.client.get('/__logo/dyko.png', headers=[
-            ('If-Modified-Since', initial_response.headers['Last-Modified']),
-        ])
-        self.assertEqual(response.status_code, 304)
-        self.assertEqual(response.data, '')
-
-    def test_logo_etag_and_last_modified(self):
-        # assume that self.test_logo() passed
-        initial_response = self.client.get('/__logo/dyko.png')
-        
-        response = self.client.get('/__logo/dyko.png', headers=[
-            ('If-None-Match', initial_response.headers['ETag']),
-            ('If-Modified-Since', initial_response.headers['Last-Modified']),
-        ])
-        self.assertEqual(response.status_code, 304)
-        self.assertEqual(response.data, '')
-
 
---
+++ test/kraken/simple_requests.py
@@ -0,0 +1,78 @@
+
+from unittest import TestCase
+
+from test.kraken import KrakenSiteMixin
+
+class TestSimpleRequests(KrakenSiteMixin, TestCase):
+    def test_notfound(self):
+        r = self.client.get('/nonexistent')
+        self.assertEqual(r.status_code, 404)
+
+    def test_static_notfound(self):
+        r = self.client.get('/__logo/inexistent.png')
+        self.assertEqual(r.status_code, 404)
+
+    def test_lipsum_notfound(self):
+        r = self.client.get('/lorem/ipsum/dolor')
+        self.assertEqual(r.status_code, 404)
+
+    def test_index(self):
+        r = self.client.get('/')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.headers['Content-Type'], 'text/html; charset=utf-8')
+        self.assertEqual(r.data, '<!DOCTYPE html PUBLIC '
+                         '"-//W3C//DTD HTML 4.01//EN" '
+                         '"http://www.w3.org/TR/html4/strict.dtd">\n'
+                         '<html><body>Dyko root</body></html>')
+
+    def test_hello(self):
+        r = self.client.get('/hello')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.headers['Content-Type'], 'text/html; charset=utf-8')
+        self.assertEqual(r.data, '<html><body>Hello, World!</body></html>')
+
+    def test_lipsum(self):
+        r = self.client.get('/lorem/ipsum')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.headers['Content-Type'], 'text/plain; charset=utf-8')
+        self.assert_('Lorem ipsum dolor sit amet' in r.data)
+
+    def test_logo(self):
+        r = self.client.get('/__logo/dyko.png')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.headers['Content-Type'], 'image/png')
+        # Maybe check the actual content here instead of just the length ?
+        self.assertEqual(len(r.data), 12677)
+
+    def test_logo_etag(self):
+        # assume that self.test_logo() passed
+        initial_response = self.client.get('/__logo/dyko.png')
+        
+        response = self.client.get('/__logo/dyko.png', headers=[
+            ('If-None-Match', initial_response.headers['ETag']),
+        ])
+        self.assertEqual(response.status_code, 304)
+        self.assertEqual(response.data, '')
+
+    def test_logo_last_modified(self):
+        # assume that self.test_logo() passed
+        initial_response = self.client.get('/__logo/dyko.png')
+        
+        response = self.client.get('/__logo/dyko.png', headers=[
+            ('If-Modified-Since', initial_response.headers['Last-Modified']),
+        ])
+        self.assertEqual(response.status_code, 304)
+        self.assertEqual(response.data, '')
+
+    def test_logo_etag_and_last_modified(self):
+        # assume that self.test_logo() passed
+        initial_response = self.client.get('/__logo/dyko.png')
+        
+        response = self.client.get('/__logo/dyko.png', headers=[
+            ('If-None-Match', initial_response.headers['ETag']),
+            ('If-Modified-Since', initial_response.headers['Last-Modified']),
+        ])
+        self.assertEqual(response.status_code, 304)
+        self.assertEqual(response.data, '')
+
+
