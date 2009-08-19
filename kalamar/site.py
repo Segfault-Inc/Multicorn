3e66a5954b78a00a693578b740cb27ed7bff1ebf
Samuel de Framond <sdeframond@kozea.fr>
2009-08-19 13:13:41 +0200

    New request parser. Breaks compatibility !!! (Tests don't pass)

--- kalamar/site.py
+++ kalamar/site.py
@@ -29,6 +29,7 @@ import warnings
 from kalamar.storage import base
 from kalamar import utils
 from kalamar import Item
+from kalamar import requestparser
 
 
 class Site(object):
@@ -81,18 +82,19 @@ class Site(object):
          Condition(u'c', <built-in function ge>, u'3')]
 
         """
-        request = unicode(request)
-        for part in request.split(u'/'):
-            if not part:
-                continue
-            for operator_str, operator_func in utils.operators.items():
-                if operator_str in part:
-                    name, value = part.split(operator_str, 1)
-                    yield utils.Condition(name, operator_func, value)
-                    break
-            else:
-                # No operator found
-                yield utils.Condition(None, None, part)
+        return requestparser.iparse(request)
+#        request = unicode(request)
+#        for part in request.split(u'/'):
+#            if not part:
+#                continue
+#            for operator_str, operator_func in utils.operators.items():
+#                if operator_str in part:
+#                    name, value = part.split(operator_str, 1)
+#                    yield utils.Condition(name, operator_func, value)
+#                    break
+#            else:
+#                # No operator found
+#                yield utils.Condition(None, None, part)
         
     def isearch(self, access_point, request):
         """Return a generator of all items in "access_point" matching "request".
