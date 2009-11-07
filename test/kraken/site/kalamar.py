d76ad4c467626522c789ba16f9fa4ae2534a150a
Simon Sapin <simon.sapin@exyr.org>
2009-11-07 18:32:46 +0100

    Fix kraken+kalamar test

--- test/kraken/site/kalamar.py
+++ test/kraken/site/kalamar.py
@@ -5,7 +5,7 @@ def handle_request(request, remaining_path):
     result = request.kalamar.search(u'fs_text_mixed', remaining_path)
     return Response(u'\n'.join(sorted(
         repr(dict(
-            (key, item.properties[key])
+            (key, item[key])
             for key in (u'genre', u'artiste', u'album', u'titre')
         ))
         for item in result
