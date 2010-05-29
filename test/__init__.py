4949736e879cee89e86d2792815f74449cc689f5
Simon Sapin <simon.sapin@exyr.org>
2010-05-29 17:22:51 +0200

    Fix coverage. We do not need to list every file anymore, we just whitelist the project dir and exclude everything else. (We do not want to see how much of our dependencies are covered, they should be tested separately.)

--- test/__init__.py
+++ test/__init__.py
@@ -5,25 +5,52 @@ import doctest
 import unittest
 import unittest2
 
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
+
 # These packages are tested by default
 DYKO_PACKAGES = ['kalamar', 'koral', 'kraken', 'test']
 
-def run_with_coverage(suite):
-    import coverage
-    try:
-        # Coverage v3 API
-        c = coverage.coverage()
-    except coverage.CoverageException:
-        # Coverage v2 API
-        c = coverage
 
+def monkeypatch_coverage():
+    """
+    coverage can blacklist some filename prefixes, but we want to whitelist
+    our project directory (and exclude everything else).
+    Monkey-patch an internal function in coverage to do that.
+    """
+    
+    from coverage.report import Reporter
+    
+    if hasattr(Reporter, '__original_find_code_units'):
+        # Safeguard so that this runs only once.
+        # Monkey-patching multiple times won’t break anything but will do the
+        # filtering multiple times uselessly
+        return
+    
+    Reporter.__original_find_code_units = Reporter.find_code_units
+    
+    def patched_find_code_units(self, *args, **kwargs):
+        self.__original_find_code_units(*args, **kwargs)
+        self.code_units = [
+            cu for cu in self.code_units
+            if cu.filename.startswith(PROJECT_DIR) and not
+               # exclude buildout-installed dependencies
+               cu.filename.startswith(os.path.join(PROJECT_DIR, 'eggs'))
+        ]
+    
+    Reporter.find_code_units = patched_find_code_units
+
+def run_with_coverage(run_function):
+    monkeypatch_coverage()
+    import coverage
+    c = coverage.coverage()
     c.exclude('return NotImplemented')
     c.exclude('raise NotImplementedError')
     c.exclude('except ImportError:')
     c.start()
-    run_suite(suite)
+    run_function()
     c.stop()
-    c.report(list(set(filenames_from_tests(suite))))
+    c.report()
 
 
 def profile(function, filename):
@@ -50,30 +77,22 @@ class DoctestLoader(unittest2.TestLoader):
 def make_suite(names=None):
     """Build a test suite each from each package, module, 
     test case class or method name."""
-    project_dir = os.path.dirname(os.path.dirname(__file__))
     suite = unittest2.TestSuite()
     loader = DoctestLoader()
     for name in names or sys.argv[1:] or DYKO_PACKAGES:
         # Try unittest2’s discovery
         try:
-            suite.addTest(loader.discover(name, '*.py', project_dir))
+            suite.addTest(loader.discover(name, '*.py', PROJECT_DIR))
         except ImportError:
             # name could be a class or a method
             suite.addTest(loader.loadTestsFromName(name))
     return suite
 
-def filenames_from_tests(test):
-    if isinstance(test, (unittest2.TestSuite, unittest.TestSuite)):
-        for t in test._tests:
-            for filename in filenames_from_tests(t):
-                yield filename
-    elif isinstance(test, doctest.DocTestCase):
-        yield test._dt_test.filename
-    elif hasattr(type(test), '__file__'):
-        # Assume a TestCase subclass instance
-        yield type(test).__file__
 
 def run_suite(suite):
+    """
+    Run a test suite with output buferring and ctrl-C catching
+    """
     # Control-C during the test run waits for the current test to end and then
     # Same as --catch :
     # reports all the results so far. A second control-C raises the normal
@@ -87,8 +106,7 @@ def main():
 
 def main_coverage():
     print "Running tests with coverage."
-    suite = make_suite()
-    run_with_coverage(suite)
+    run_with_coverage(main)
 
 def main_profile(filename='./profile_results'):
     print "Profiling tests."
