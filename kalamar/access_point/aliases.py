64488b69c224b31a13e2d9ca22d144c52c4a56db
Simon Sapin <simon.sapin@exyr.org>
2010-09-18 02:12:02 +0200

    Add Aliases wrapper access point.

---
+++ kalamar/access_point/aliases.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+# This file is part of Dyko
+# Copyright © 2008-2009 Kozea
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Kalamar.  If not, see <http://www.gnu.org/licenses/>.
+
+from .base import AccessPoint
+from .. import item
+from ..request import Condition, And, Or, Not
+
+
+class AliasedItem(item.MutableMultiMapping):
+    def __init__(self, access_point, underlying_item):
+        self.access_point = access_point
+        self.underlying_item = underlying_item
+
+    def __repr__(self):
+        return 'AliasedItem(%r, %r)' % (self.access_point,
+                                        self.underlying_item)
+    
+    def _translate_key(self, key):
+        if key not in self.access_point.properties:
+            raise KeyError
+        return self.access_point.aliases.get(key, key)
+    
+    def getlist(self, key):
+        return self.underlying_item.getlist(self._translate_key(key))
+    
+    def setlist(self, key, values):
+        return self.underlying_item.setlist(self._translate_key(key), values)
+    
+    def __delitem__(self, key):
+        raise TypeError("%s object doesn't support item deletion." %
+            self.__class__.__name__)
+
+    def __iter__(self):
+        for key in self.underlying_item:
+            yield self.access_point.reversed_aliases.get(key, key)
+
+    def __len__(self):
+        return len(self.underlying_item)
+
+    # default to underlying_item for all other methods and attributes
+    def __getattr__(self, name):
+        return getattr(self.underlying_item, name)
+
+
+class Aliases(AccessPoint):
+    """
+    This access point wrapper renames properties.
+    """
+    def __init__(self, aliases, wrapped_ap):
+        """
+        :param aliases: A dict where keys are the new property names,
+            and values are the names in the wrapped access point.
+        """
+        self.wrapped_ap = wrapped_ap
+        self.aliases = aliases
+        self.reversed_aliases = dict((v, k) for k, v in self.aliases.items())
+        self.properties = dict(
+            (self.reversed_aliases.get(name, name), property)
+            for name, property in wrapped_ap.properties.items())
+    
+    def translate_request(self, request):
+        if isinstance(request, And):
+            return And(*(self.translate_request(r)
+                         for r in request.sub_requests))
+        elif isinstance(request, Or):
+            return Or(*(self.translate_request(r)
+                        for r in request.sub_requests))
+        elif isinstance(request, Not):
+            return Not(self.translate_request(request.sub_request))
+        elif isinstance(request, Condition):
+            name = request.property_name
+            return Condition(self.aliases.get(name, name),
+                             request.operator,
+                             request.value)
+        else:
+            raise ValueError('Unknown request type : %r' % request)
+    
+    def search(self, request):
+        request = self.translate_request(request)
+        for underlying_item in self.wrapped_ap.search(request):
+            yield AliasedItem(self, underlying_item)
+    
+    def delete(self, item):
+        self.wrapped_ap.delete(item.underlying_item)
+    
+    def save(self, item):
+        self.wrapped_ap.save(item.underlying_item)
+
---
+++ kalamar/tests/test_aliases.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+# This file is part of Dyko
+# Copyright © 2008-2009 Kozea
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Kalamar.  If not, see <http://www.gnu.org/licenses/>.
+
+from nose.tools import eq_, nottest
+from kalamar import Site, Item
+from kalamar.request import Condition, And, Or, Not, Request
+from kalamar.access_point.memory import Memory
+from kalamar.access_point.aliases import AliasedItem, Aliases
+from .test_memory import make_test_site
+
+class DummyAP(object):
+    pass
+
+def test_aliased_item():
+    dummy_underlying_ap = DummyAP()
+    dummy_underlying_ap.properties = {'FOO': None, 'other': None}
+    dummy_ap = DummyAP()
+    dummy_ap.properties = {'foo': None, 'other': None}
+    dummy_ap.aliases = {'foo': 'FOO'}
+    dummy_ap.reversed_aliases = {'FOO': 'foo'}
+    underlying_item = Item(dummy_underlying_ap, {'FOO': 9, 'other': 0})
+    item = AliasedItem(dummy_ap, underlying_item)
+    # aliased property
+    eq_(underlying_item['FOO'], 9)
+    item['foo'] = 1
+    eq_(underlying_item['FOO'], 1)
+    # old names are masked
+    try:
+        item['FOO']
+    except KeyError:
+        pass
+    else:
+        assert False, 'expected KeyError'
+    # non aliased property
+    eq_(underlying_item['other'], 0)
+    item['other'] = 2
+    eq_(underlying_item['other'], 2)
+    # other attr
+    assert item.access_point is dummy_ap
+    # MultiDict-like method, aliased property
+    item.setlist('foo', (2, 3))
+    eq_(item['foo'], 2)
+    eq_(item.getlist('foo'), (2, 3))
+    eq_(underlying_item['FOO'], 2)
+    eq_(underlying_item.getlist('FOO'), (2, 3))
+
+
+def test_translate_request():
+    ap = Aliases({'foo': 'FOO', 'bar': 'BAR'}, Memory({}, ''))
+    C = Condition
+    eq_(ap.translate_request(C('foo', '=', 4)), C('FOO', '=', 4))
+    eq_(ap.translate_request(C('other', '=', 7)), C('other', '=', 7))
+    eq_(
+        ap.translate_request(And(
+            C('foo', '=', 4),
+            Not(Or(C('other', '=', 7), C('bar', '!=', 1)))
+        )),
+        And(
+            Condition('FOO', '=', 4), 
+            Not(Or(C('other', '=', 7), C('BAR', '!=', 1)))
+        )
+    )
+
+
+def test_aliased_memory():
+    site = make_test_site()
+    underlying_ap = site.access_points['things']
+    ap = Aliases({'nom': 'name'}, underlying_ap)
+    site.register('aliased', ap)
+    
+    results = site.search('aliased', {'nom': 'bar'})
+    eq_(set(item['id'] for item in results), set([2, 3]))
+
+    # old names are masked
+    try:
+        site.search('aliased', {'name': 'bar'})
+    except KeyError:
+        pass
+    else:
+        assert False, 'KeyError expected.'
+
