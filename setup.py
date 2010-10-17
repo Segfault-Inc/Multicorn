152b80be2c4d8745679bb9c63db6eac32bbbe39a
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2010-10-17 02:22:11 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -4,26 +4,26 @@
 
 from setuptools import setup, find_packages
 
+
 setup(
-    name = 'Dyko',
-    version = '0.2.dev1',
+    name = "Dyko",
+    version = "0.2.dev1",
     packages = find_packages(
-        exclude=['*._test', '*._test.*', 'test.*', 'test']),
+        exclude=["*._test", "*._test.*", "test.*", "test"]),
     package_dir = {
-        'kalamar': 'kalamar',
-        'koral': 'koral',
-        'kraken': 'kraken'},
+        "kalamar": "kalamar",
+        "koral": "koral",
+        "kraken": "kraken"},
     package_data = {
-        '': ['AUTHORS'],
-        'doc': ['*.rst']},
+        "": ["AUTHORS"],
+        "doc": ["*.rst"]},
     install_requires = [
-        'werkzeug>=0.5',
-        # TODO: test sqlalchemy 0.5?
-        'sqlalchemy>=0.6'],
+        "werkzeug>=0.5"],
     extras_require = {
-        'Genshi': ['genshi>=0.5'],
-        'Jinja2': ['jinja2>=2.0'],
-        'Ogg/Vorbis': ['mutagen>=1.16']},
+        "Genshi": ["genshi>=0.5"],
+        "Mako": ["mako>=0.3"],
+        "SQLAlchemy": ["sqlalchemy>=0.6"],
+        "Jinja2": ["jinja2>=2.0"]},
     author = "Kozea",
     author_email = "guillaume.ayoub@kozea.fr",
     description = "This is a light and flexible web framework in full python.",
