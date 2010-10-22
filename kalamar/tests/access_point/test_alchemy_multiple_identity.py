5348789a09c08cf9096bba529af9e6563e5e21a6
Ronan Dunklau <ronan.dunklau@kozea.fr>
2010-10-22 14:30:36 +0200

    Add forgotten files

---
+++ kalamar/tests/access_point/test_alchemy_multiple_identity.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+# This file is part of Dyko
+# Copyright © 2008-2010 Kozea
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Kalamar.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Alchemy test
+============
+
+Test the alchemy backend on an sqlite base.
+
+"""
+
+from nose.tools import eq_, nottest
+from kalamar.access_point.alchemy import AlchemyProperty, Alchemy
+from kalamar.site import Site
+from datetime import date
+
+
+@nottest
+def make_testtable():
+    """Returns a simple access point"""
+    Alchemy.__metadatas = {}
+    firstname = AlchemyProperty(unicode, column_name="firstname")
+    lastname = AlchemyProperty(unicode, column_name="lastname")
+    birthdate = AlchemyProperty(date, column_name="birthdate")
+    access_point = Alchemy("sqlite:///", "test_multiple_keys", {
+        "firstname": firstname,
+        "lastname": lastname,
+        "birthdate" : birthdate},
+        ["firstname","lastname"], True)
+    access_point._table
+    return access_point
+
+
+class TestAlchemy(object):
+    """Class defining some simple tests on an Alchemy access point"""
+    def setUp(self):
+        """Setup the class for the tests, creating the kalamar site and
+        populating it with test data"""
+        self.site = Site()
+        self.access_point = make_testtable()
+        self.site.register("test", self.access_point) 
+        self.items = []
+        item = self.site.create("test", {"firstname": "John", "lastname": u"Doe",
+                "birthdate" : date(1950,1,1)})
+        self.items.append(item)
+        item.save()
+        item = self.site.create("test", {"firstname": "Jane", "lastname": u"Doe",
+                "birthdate" : date(1960,2,2)})
+        self.items.append(item)
+        item.save()
+
+    def testsearch(self):
+        """Tests a simple search on the access point"""
+        items = list(self.site.search("test"))
+        eq_(len(items), 2)
+        items = list(self.site.search("test", {"firstname": "John"}))
+        eq_(len(items), 1)
+        item = items[0]
+        eq_(item["firstname"], "John")
+        eq_(item["lastname"], "Doe")
+        eq_(item["birthdate"], date(1950,1,1))
+
+    def testview(self):
+        """Test a simple view on the access point"""
+        items = list(
+            self.site.view("test", {"truc": "firstname", "name": u"lastname"}, {}))
+        eq_(len(items), 2)
+        for item in items:
+            assert "truc" in item.keys() and "name" in item.keys()
+        items = list(
+            self.site.view("test", {"truc": "firstname", "name": u"lastname"},
+                {"firstname": "John"}))
+        eq_(len(items), 1)
+
+    def testupdate(self):
+        """Assert that an item can be updated in the DB"""
+        item = self.site.open("test", {"firstname": "John", "lastname" : "Doe"})
+        item["birthdate"] = date(1951,12,12)
+        item.save()
+        item = self.site.open("test", {"firstname": "John", "lastname" : "Doe"})
+        eq_(item["birthdate"],  date(1951,12,12))
+
+    def tearDown(self):
+        """Tears the class down between tests"""
+        for access_point in self.site.access_points.values(): 
+            access_point._table.drop()
---
+++ kraken/tests/controllers.py
@@ -0,0 +1,25 @@
+from kraken.site import expose_template
+
+@expose_template("/helloparam/<string:message>/")
+def helloparam(request, message, **kwargs):
+    return {"message" : message}
+
+@expose_template("/methods/",methods=("GET",))
+def getmethod(request, **kwargs):
+    return {"message": "GET world"}
+
+@expose_template("/methods/",methods=("POST",))
+def postmethod(request, **kwargs):
+    return {"message": "POST world"}
+
+@expose_template()
+def hello(request, **kwargs):
+    return {'request' : request}
+
+@expose_template("/another/template/<string:message>", template="helloparam")
+def anothertemplate(request, message, **kwargs):
+    return {'message': message}
+
+@expose_template("/<string:hello>/message")
+def weirdpath(request, hello, **kwargs):
+    return {'message': hello} 
