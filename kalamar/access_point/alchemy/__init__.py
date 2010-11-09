48f7633202ade2662e7364168232dbe2b553ee1a
Ronan Dunklau <ronan.dunklau@kozea.fr>
2010-11-09 10:00:19 +0100

    Fixed a bug and the test for "properties_tree"

--- kalamar/access_point/alchemy/__init__.py
+++ kalamar/access_point/alchemy/__init__.py
@@ -124,7 +124,7 @@ class Alchemy(AccessPoint):
         """Initialize the Alchemy engine on first access."""
         metadata = Alchemy.__metadatas.get(self.url, None)
         if not metadata:
-            engine = create_engine(self.url, echo=True)
+            engine = create_engine(self.url, echo=False)
             metadata = MetaData()
             metadata.bind = engine
             Alchemy.__metadatas[self.url] = metadata
