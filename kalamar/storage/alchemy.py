2e53632898ff4ee5940afa82ccb7fd9ccf5e906d
Ronan Dunklau <ronan.dunklau@kozea.fr>
2010-12-21 11:53:40 +0100

    debug

--- kalamar/storage/alchemy.py
+++ kalamar/storage/alchemy.py
@@ -104,8 +104,9 @@ class AlchemyAccessPoint(AccessPoint):
                 self.columns[name] = column
             else :
                 self.one_to_manies[name] = props.get('foreign-property',self.name)
-        except:
+        except Exception as detail:
             print "A problem occured while creating config for ap %s with column %s " % (self.name, name)
+            print detail
             raise
 
 
