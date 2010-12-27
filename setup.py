9ba9f711b032db48d8ed6147baa13b026707008c
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2010-12-27 18:35:01 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -1,33 +1,70 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
+#
 # Public Domain
+
+"""
+Dyko - A Light Web Framework
+============================
+
+Dyko is a light web framework with original features:
+
+- Support for **multiple storage mechanisms** (databases, filesystems, and
+  more) with an **unified data access interface**, giving the possibility to
+  use **flexible and evolutive data models**;
+- Support for **multiple template engines** (Genshi, Jinja, Mako, and more).
+
+Theses specificities make Dyko particularly suitable for creating:
+
+- Small and simple websites with no database;
+- Content management systems with heterogeneous storage engines;
+- Web applications with chronic data model evolutions;
+- Web applications accessing static data models defined by other applications.
+
+Dyko should run on most of the UNIX-like platforms (Linux, BSD, MacOS X) and
+Windows. It is free and open-source software, written in Python, released under
+GPL version 3.
+
+For further information, please visit the `Dyko Website
+<http://www.dyko.org/>`_.
+
+"""
 
 from setuptools import setup, find_packages
 
 
 setup(
-    name = "Dyko",
-    version = "0.2.dev1",
-    packages = find_packages(
+    name="Dyko",
+    version="0.2.dev1",
+    description="A Light Web Framework",
+    long_description=__doc__,
+    author="Kozea",
+    author_email="guillaume.ayoub@kozea.fr",
+    url="http://www.dyko.org/",
+    license="GNU GPL v3",
+    platforms="Any",
+    packages=find_packages(
         exclude=["*._test", "*._test.*", "test.*", "test"]),
-    package_dir = {
-        "kalamar": "kalamar",
-        "kraken": "kraken"},
-    package_data = {
-        "": ["AUTHORS"],
+    provides=["kalamar", "kraken"],
+    package_data={
         "kalamar": ["access_point/xml/xml2rst.xsl"]},
-    extras_require = {
-        "werkzeug": ["werkzeug>=0.5"],
+    extras_require={
+        "Werkzeug": ["werkzeug>=0.5"],
         "docutils": ["docutils>=0.6"],
         "lxml": ["lxml>=2.0"],
         "Genshi": ["genshi>=0.5"],
         "Mako": ["mako>=0.3"],
         "SQLAlchemy": ["sqlalchemy>=0.6"],
         "Jinja2": ["jinja2>=2.0"]},
-    author = "Kozea",
-    author_email = "guillaume.ayoub@kozea.fr",
-    description = "This is a light and flexible web framework in full python.",
-    license = "GPL",
-    keywords = "web framework database",
-    url = "http://www.dyko.org/",
-    zip_safe=False)
+    keywords=["web", "framework", "database"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Application Frameworks"])
