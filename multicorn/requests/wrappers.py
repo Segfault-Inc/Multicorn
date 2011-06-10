5f61d30d6a19254fba1e980ba9ffe67c9766e554
Ronan Dunklau <ronan.dunklau@kozea.fr>
2011-06-10 14:56:35 +0200

    Added wrapper for type inference

--- multicorn/requests/wrappers.py
+++ multicorn/requests/wrappers.py
@@ -1,3 +1,5 @@
+import sys
+
 from . import requests
 from .types import Type, Dict, List
 
@@ -24,8 +26,8 @@ class RequestWrapper(object):
         self.wrapped_request = wrapped_request
         self.args = wrapped_request._Request__args
 
-    def resultingtypes(self):
-        raise NotImplementedError("Resultingtypes is not implemented")
+    def return_type(self, contexts=()):
+        raise NotImplementedError("return_type is not implemented")
 
 
 
@@ -35,7 +37,7 @@ class LiteralWrapper(RequestWrapper):
         super(LiteralWrapper, self).__init__(*args, **kwargs)
         self.value, = self.args
 
-    def resultingtypes(self):
+    def return_type(self, contexts=()):
         return Type(type=type(self.value))
 
 
@@ -46,7 +48,7 @@ class ListWrapper(RequestWrapper):
         self.value, = self.args
         self.value = [self.from_request(r) for r in self.value]
 
-    def resultingtypes(self):
+    def return_type(self, contexts=()):
         return Type(type=list)
 
 
@@ -58,7 +60,7 @@ class TupleWrapper(RequestWrapper):
         self.value, = self.args
         self.value = tuple(self.from_request(r) for r in self.value)
 
-    def resultingtypes(self):
+    def return_type(self, contexts=()):
         return Type(type=tuple)
 
 
@@ -72,7 +74,7 @@ class DictWrapper(RequestWrapper):
             (key, self.from_request(value))
             for key, value in self.value.iteritems())
 
-    def resultingtypes(self):
+    def return_type(self, contexts=()):
         return Type(type=dict)
 
 
@@ -98,20 +100,143 @@ class OperationWrapper(RequestWrapper):
         assert (self.method_name and not self.operator_name) or (
                 self.operator_name and not self.method_name)
 
-@RequestWrapper.register_wrapper(requests.GetattrOperation)
-class GetattrOperationWrapper(OperationWrapper):
+@RequestWrapper.register_wrapper(requests.GetattrRequest)
+class GetattrWrapper(OperationWrapper):
 
     def __init__(self, *args, **kwargs):
-        super(GetattrOperationWrapper, self).__init__(*args, **kwargs)
+        super(GetattrWrapper, self).__init__(*args, **kwargs)
         self.subject = self.args[0]
         self.key = self.args[1]
 
-    def resultingtypes(self):
-        initial_types = self.subject.resultingtypes()
+    def return_type(self, contexts=()):
+        initial_types = self.subject.return_type(contexts)
         if isinstance(initial_types, Dict):
             return initial_types.mapping[self.key]
         else:
-            #If the subject is not an item-like, can't infer anything
+            # If the subject is not an item-like, can't infer anything
+            return Type(type=object)
+
+class BooleanOperationWrapper(OperationWrapper):
+
+    def return_type(self, contexts=()):
+        return Type(type=bool)
+
+BOOL_OPERATORS = ('and', 'or', 'xor', 'contains', 'eq', 'ne',
+    'lt', 'gt', 'le', 'ge')
+
+def defclass(operator, base_class):
+    class_name = operator.title() + 'Wrapper'
+    request_class = getattr(requests, operator.title() + 'Request')
+    class_ = type(class_name, (base_class,), {})
+    class_ = RequestWrapper.register_wrapper(request_class)
+    setattr(sys.modules[__name__], class_name, class_)
+
+
+
+for operator in BOOL_OPERATORS:
+   defclass(operator, BooleanOperationWrapper)
+
+ARITHMETIC_OPERATORS = ('add', 'sub', 'mul', 'floordiv', 'div', 'truediv', 'pow', 'mod')
+
+class ArithmeticOperationWrapper(OperationWrapper):
+
+    def return_type(self, contexts=()):
+        left_type = self.args[0].return_type(contexts)
+        right_type = self.args[0].return_type(contexts)
+        if left_type.type == right_type.type:
+            return Type(type=left_type.type)
+        else:
+            # TODO: refine type inference for heterogeneous types
             return Type(type=object)
 
 
+for operator in BOOL_OPERATORS:
+   defclass(operator, ArithmeticOperationWrapper)
+
+@RequestWrapper.register_wrapper(requests.FilterRequest)
+class FilterWrapper(RequestWrapper):
+
+    def __init__(self, *args, **kwargs):
+        super(FilterWrapper, self)
+        self.subject = self.args[0]
+        self.predicate = self.args[1]
+
+    def return_type(self, contexts=()):
+        # A filter does not modify its subject
+        # assert self.predicate.return_type().type == bool
+        return self.subject.return_type(contexts + (self.subject,))
+
+
+@RequestWrapper.register_wrapper(requests.MapRequest)
+class MapWrapper(RequestWrapper):
+
+    def __init__(self, *args, **kwargs):
+        super(MapWrapper, self).__init__(self, *args, **kwargs)
+        self.suject = self.args[0]
+        self.operation = self.args[1]
+
+    def return_type(self, contexts=()):
+        return self.operation.return_type(contexts + (self.subject,))
+
+
+@RequestWrapper.register_wrapper(requests.SortRequest)
+class GroupbyWrapper(RequestWrapper):
+
+    def __init__(self, *args, **kwargs):
+        super(GroupbyWrapper, self).__init__(self, *args, **kwargs)
+        self.subject = self.args[0]
+
+    def return_type(self, contexts=()):
+        subject_type = self.subject.return_type(contexts)
+        key_type = self.key.return_type(contexts + (self.subject,))
+        return Dict(mapping={'grouper': subject_type,
+            'elements': List(inner_type=key_type)})
+
+
+class PreservingWrapper(RequestWrapper):
+
+    def __init__(self, *args, **kwargs):
+        super(PreservingWrapper, self).__init__(*args, **kwargs)
+        self.subject = self.args[0]
+
+    def return_type(self, contexts=()):
+        return self.suject.return_type(contexts)
+
+
+@RequestWrapper.register_wrapper(requests.DistinctRequest)
+class DistinctWrapper(PreservingWrapper):
+    pass
+
+
+@RequestWrapper.register_wrapper(requests.SortRequest)
+class SortWrapper(PreservingWrapper):
+    pass
+
+@RequestWrapper.register_wrapper(requests.MaxRequest)
+class MaxWrapper(PreservingWrapper):
+    pass
+
+@RequestWrapper.register_wrapper(requests.MinRequest)
+class MinWrapper(PreservingWrapper):
+    pass
+
+@RequestWrapper.register_wrapper(requests.CountRequest)
+class CountWrapper(RequestWrapper):
+
+    def return_type(self, contexts=()):
+        return Type(type=int)
+
+@RequestWrapper.register_wrapper(requests.SumRequest)
+class SumWrapper(RequestWrapper):
+    def __init__(self, *args, **kwargs):
+        super(SumWrapper, self).__init__(*args, **kwargs)
+        self.subject = self.args[0]
+
+    def return_type(self, contexts=()):
+        subject_type = self.subject.return_type(contexts)
+        if isinstance(subject_type, List):
+            # Is sum supposed to keep the type ?
+            return subject_type.inner_type
+        else:
+            # If this really supposed to happen?
+            return Type(type=object)
