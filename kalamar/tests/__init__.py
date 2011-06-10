b18b7a0bb05f136cff3a33ecb956d82e6b50019f
Ronan Dunklau <ronan.dunklau@kozea.fr>
2011-06-10 15:16:22 +0200

    Removed kalamar!

--- kalamar/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright © 2008-2011 Kozea
-# This file is part of Multicorn, licensed under 3-clause BSD
-
-"""
-Multicorn test suite.
-
-"""
-
--- kalamar/tests/combinations/access_point/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright © 2008-2011 Kozea
-# This file is part of Multicorn, licensed under 3-clause BSD
-
-"""
-Access point definitions for tests combinations.
-
-"""
