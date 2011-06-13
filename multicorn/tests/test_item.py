08f5b2798299c43ca43ffe6ecaa6cefbbe3703f3
Florian Mounier <florian.mounier@kozea.fr>
2011-06-13 13:20:43 +0200

    Change lazyloader to no cache + tests

--- multicorn/tests/test_item.py
+++ multicorn/tests/test_item.py
@@ -18,37 +18,77 @@ def test_item():
         pass
     corn = Dummy()
     corn.properties = [BaseProperty(name, int)
-                       for name in ('foo', 'bar', 'baz')]
-    
+                       for name in (
+                           'foo', 'bar', 'baz', 'fuu')]
+
     def baz_loader():
         baz_loader.calls += 1
         return 8
 
+    class Fuu_loader():
+        value = None
+
+        def __call__(self):
+            if self.value is None:
+                self.calls += 1
+                self.value = 10 + 2
+            return self.value
+
+    fuu_loader = Fuu_loader()
+    fuu_loader.calls = 0
+
     baz_loader.calls = 0
-    item = BaseItem(corn, {'foo': 3, 'bar': 5}, {'baz': baz_loader})
-    assert len(item) == 3
-    assert set(item) == set(['foo', 'bar', 'baz'])
+    item = BaseItem(corn,
+                    {'foo': 3, 'bar': 5},
+                    {'baz': baz_loader, 'fuu': fuu_loader})
+    assert len(item) == 4
+    assert set(item) == set(['foo', 'bar', 'baz', 'fuu'])
     assert 'bar' in item
     assert 'baz' in item
+    assert 'fuu' in item
     assert 'Lipsum' not in item
     assert item['foo'] == 3
     # The loader was not called yet.
     assert baz_loader.calls == 0
-    assert item['baz'] == 8
+    # With assert_hooks, this is evaluated twice
+    # Storing in a variable
+    baz = item['baz']
+    assert baz == 8
     # Get a second time.
     assert item['foo'] == 3
+    baz = item['baz']
+    assert baz == 8
+    # The loader was called twice.
+    assert baz_loader.calls == 2
+
+    # The loader was not called yet.
+    assert fuu_loader.calls == 0
+    assert item['fuu'] == 12
     assert item['baz'] == 8
-    # The loader was called only once.
-    assert baz_loader.calls == 1
+    # Get a second time.
+    assert item['fuu'] == 12
+    assert item['foo'] == 3
+    assert item['fuu'] == 12
+
+    # The loader was called once.
+    assert fuu_loader.calls == 1
 
     baz_loader.calls = 0
-    item = BaseItem(corn, {'foo': 3, 'bar': 5}, {'baz': baz_loader})
+    fuu_loader.calls = 0
+    item = BaseItem(corn,
+                    {'foo': 3, 'bar': 5},
+                    {'baz': baz_loader, 'fuu': fuu_loader})
+
     item['baz'] = 13
     assert item['baz'] == 13
+    item['fuu'] = 29
+    assert item['fuu'] == 29
     item['foo'] = 21
     assert item['foo'] == 21
-    # The loader was never called
+
+    # The loaders were never called
     assert baz_loader.calls == 0
+    assert fuu_loader.calls == 0
 
     with attest.raises(KeyError):
         item['Lorem'] = 33
@@ -57,4 +97,3 @@ def test_item():
         del item['foo']
     with attest.raises(TypeError):
         del item['ispum']
-
