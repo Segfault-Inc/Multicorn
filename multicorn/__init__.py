00cdb6241c2c43f49488f8f145b3fffe102756dd
Florian Mounier <florian.mounier@kozea.fr>
2011-06-13 11:47:59 +0200

    Never cache the value + pep8

--- multicorn/__init__.py
+++ multicorn/__init__.py
@@ -2,11 +2,12 @@
 # Copyright © 2008-2011 Kozea
 # This file is part of Multicorn, licensed under a 3-clause BSD license.
 
+
 class Multicorn(object):
     def __init__(self):
         self.corns = {}
 
     def register(self, corn):
-        corn.bind(self) # Do this first as it may raise.
+        corn.bind(self)  # Do this first as it may raise.
         self.corns[corn.name] = corn
         return corn
--- multicorn/item.py
+++ multicorn/item.py
@@ -8,10 +8,10 @@ from collections import MutableMapping
 class BaseItem(MutableMapping):
     """
     Base class for items.
-    
+
     :param corn: The corn this item is coming from
     :param values: A dict of {property name: value}
-    :param lazy_values: A dict of {property name: loader} where loader is a 
+    :param lazy_values: A dict of {property name: loader} where loader is a
                         callable returning the actual value. This can be used
                         for values that are expensive to compute or get.
     """
@@ -27,7 +27,7 @@ class BaseItem(MutableMapping):
     def __iter__(self):
         for prop in self.corn.properties:
             yield prop.name
-    
+
     def __contains__(self, key):
         # MutableMapping.__contains__ would work but is based on __getitem__
         # which may load a lazy value needlessly.
@@ -35,16 +35,14 @@ class BaseItem(MutableMapping):
 
     def __getitem__(self, key):
         if key not in self._values and key in self._lazy_values:
-            self._values[key] = self._lazy_values.pop(key)()
+            return self._lazy_values[key]
         return self._values[key]
 
     def __setitem__(self, key, value):
-        if key not in self: # based on self.corn.properties
+        if key not in self:  # based on self.corn.properties
             raise KeyError(key, 'Can not add properties to items.')
         self._values[key] = value
         self._lazy_values.pop(key, None)
 
     def __delitem__(self, key):
         raise TypeError('Can not delete properties from an item.')
-
-
