63056b91b34b8a926ee1bd13c4ce10f9828c513f
Florian Mounier <florian.mounier@kozea.fr>
2011-06-21 09:47:58 +0200

    Add over nine thousand wrappers

--- multicorn/corns/mongo/wrapper.py
+++ multicorn/corns/mongo/wrapper.py
@@ -2,6 +2,32 @@ from ...requests import requests, wrappers
 from .request import MongoRequest
 
 
+def merge_dicts(dict1, dict2):
+    def merge_key(key, dict1, dict2):
+        if type(dict1[key]) == type(dict2[key]):
+            if isinstance(dict1[key], dict):
+                new_dict = dict(dict1[key])
+                new_dict.update(dict2[key])
+                return new_dict
+            elif isinstance(dict1[key], list):
+                new_list = list(dict1[key])
+                new_list.extend(dict2[key])
+                return new_list
+        raise
+
+    final_dict = {}
+    for key in dict1:
+        if key in dict2:
+            final_dict[key] = merge_key(key, dict1, dict2)
+        else:
+            final_dict[key] = dict1[key]
+
+    for key in dict2:
+        if key not in dict1:
+            final_dict[key] = dict2[key]
+    return final_dict
+
+
 class MongoWrapper(wrappers.RequestWrapper):
     class_map = wrappers.RequestWrapper.class_map.copy()
 
@@ -21,8 +47,73 @@ class FilterWrapper(wrappers.FilterWrapper, MongoWrapper):
 
     def to_mongo(self, contexts=()):
         expression = self.subject.to_mongo()
-        expression.spec.update(self.predicate.to_mongo())
+        expression.spec = merge_dicts(
+            expression.spec,
+            self.predicate.to_mongo())
         return expression
+
+
+@MongoWrapper.register_wrapper(requests.AndRequest)
+class AndWrapper(wrappers.AndWrapper, MongoWrapper):
+
+    def to_mongo(self, contexts=()):
+        return merge_dicts(
+            self.subject.to_mongo(contexts),
+            self.other.to_mongo(contexts))
+
+
+@MongoWrapper.register_wrapper(requests.OrRequest)
+class OrWrapper(wrappers.OrWrapper, MongoWrapper):
+
+    def to_mongo(self, contexts=()):
+        return {"$or":
+                [self.subject.to_mongo(contexts),
+                 self.other.to_mongo(contexts)]}
+
+
+@MongoWrapper.register_wrapper(requests.LeRequest)
+class LeWrapper(wrappers.LeWrapper, MongoWrapper):
+
+    def to_mongo(self, contexts=()):
+        return {self.subject.to_mongo(contexts):
+                {"$lte":
+                 self.other.to_mongo(contexts)}}
+
+
+@MongoWrapper.register_wrapper(requests.GeRequest)
+class GeWrapper(wrappers.GeWrapper, MongoWrapper):
+
+    def to_mongo(self, contexts=()):
+        return {self.subject.to_mongo(contexts):
+                {"$gte":
+                 self.other.to_mongo(contexts)}}
+
+
+@MongoWrapper.register_wrapper(requests.LtRequest)
+class LtWrapper(wrappers.LtWrapper, MongoWrapper):
+
+    def to_mongo(self, contexts=()):
+        return {self.subject.to_mongo(contexts):
+                {"$lt":
+                 self.other.to_mongo(contexts)}}
+
+
+@MongoWrapper.register_wrapper(requests.GtRequest)
+class GtWrapper(wrappers.GtWrapper, MongoWrapper):
+
+    def to_mongo(self, contexts=()):
+        return {self.subject.to_mongo(contexts):
+                {"$gt":
+                 self.other.to_mongo(contexts)}}
+
+
+@MongoWrapper.register_wrapper(requests.NeRequest)
+class NeWrapper(wrappers.NeWrapper, MongoWrapper):
+
+    def to_mongo(self, contexts=()):
+        return {self.subject.to_mongo(contexts):
+                {"$ne":
+                 self.other.to_mongo(contexts)}}
 
 
 @MongoWrapper.register_wrapper(requests.EqRequest)
