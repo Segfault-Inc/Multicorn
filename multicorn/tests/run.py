624d24f4ee0812e3a75e1ce3998098f307961246
Mounier Florian <paradoxxx.zero@gmail.com>
2011-06-26 11:47:17 -0700

    Add run.py argument for testing only a corn

--- multicorn/tests/run.py
+++ multicorn/tests/run.py
@@ -3,12 +3,18 @@
 # Copyright © 2008-2011 Kozea
 # This file is part of Multicorn, licensed under a 3-clause BSD license.
 
+import sys
 from attest import Tests
 from attest.hook import AssertImportHook
 
-def run():
+
+def run(suffix=None):
+    suffix = ".corns.%s" % suffix if suffix else ""
     with AssertImportHook():
-        Tests("multicorn.tests").main()
+        Tests("multicorn.tests%s" % suffix).main()
 
 if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        suffix = sys.argv[1]
+        run(suffix)
     run()
