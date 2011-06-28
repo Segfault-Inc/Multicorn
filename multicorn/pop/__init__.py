0b3115f29d738799a20d8beafc0777e7b56d1e79
Florian Mounier <florian.mounier@kozea.fr>
2011-06-28 16:36:15 +0200

    Pop to module

--- multicorn/pop/__init__.py
+++ multicorn/pop/__init__.py
@@ -4,49 +4,7 @@
 
 from multicorn.requests.requests import ARGUMENT_NOT_GIVEN
 from multicorn.requests import CONTEXT as c
-from collections import MutableMapping
-
-
-class AttrW(MutableMapping):
-
-    def __init__(self, wrapped):
-        self.wrapped = wrapped
-
-    def __getattr__(self, name):
-        return self.wrapped[name]
-
-    def __setattr__(self, name, value):
-        if name == "wrapped":
-            super(AttrW, self).__setattr__(name, value)
-        else:
-            self.wrapped[name] = value
-
-    def __repr__(self):
-        return self.wrapped.__repr__()
-
-    def __len__(self):
-        return self.wrapped.__len__()
-
-    def __iter__(self):
-        return self.wrapped.__iter__()
-
-    def __contains__(self, key):
-        return self.wrapped.__contains__(key)
-
-    def __getitem__(self, key):
-        return self.wrapped.__getitem__(key)
-
-    def __setitem__(self, key, value):
-        return self.wrapped.__setitem__(key, value)
-
-    def __delitem__(self, key):
-        return self.wrapped.__delitem__(key)
-
-    def save(self):
-        return self.wrapped.save()
-
-    def delete(self):
-        return self.wrapped.delete()
+from .attrwrap import AttrWrap
 
 
 class Pop(object):
@@ -59,12 +17,12 @@ class Pop(object):
         if self.lazy:
             def wgen(items):
                 for item in items:
-                    yield AttrW(item)
+                    yield AttrWrap(item)
             return wgen(pop.execute())
-        return [AttrW(item) for item in pop.execute()]
+        return [AttrWrap(item) for item in pop.execute()]
 
     def one(self, predicate=ARGUMENT_NOT_GIVEN, **kwargs):
-        return AttrW(self.corn.all.filter(
+        return AttrWrap(self.corn.all.filter(
             predicate, **kwargs).one().execute())
 
     def get(self, filter=ARGUMENT_NOT_GIVEN,
