d3623bafbb30725022dda22f13df523814778ed9
Florian Mounier <florian.mounier@kozea.fr>
2011-08-26 10:56:37 +0200

    Pep8

--- multicorn/tests/corns/alchemy.py
+++ multicorn/tests/corns/alchemy.py
@@ -9,6 +9,7 @@ from multicorn.requests import CONTEXT as c
 
 from psycopg2.extensions import STATUS_BEGIN
 
+
 def make_corn():
     @declare(Alchemy, identity_properties=("id",))
     class Corn(object):
@@ -16,6 +17,7 @@ def make_corn():
         name = Property(type=unicode)
         lastname = Property(type=unicode)
     return Corn
+
 
 def make_postgres_corn():
     @declare(Alchemy, identity_properties=("id",),
@@ -32,7 +34,9 @@ def teardown(Corn):
     Corn.table.drop()
 
 suite = make_test_suite(make_corn, 'alchemy-sqlite', teardown=teardown)
-postgres_suite = make_test_suite(make_postgres_corn, 'alchemy-postgres', teardown=teardown)
+postgres_suite = make_test_suite(
+    make_postgres_corn, 'alchemy-postgres', teardown=teardown)
+
 
 @suite.test
 @postgres_suite.test
@@ -59,14 +63,14 @@ def test_optimization(Corn):
 
     items = list(Corn.all.execute())
     assert len(items) == 3
-    items = list(Corn.all.filter(c.name == 'foo' ).execute())
+    items = list(Corn.all.filter(c.name == 'foo').execute())
     assert len(items) == 2
     assert all(item['name'] == 'foo' for item in items)
-    items = list(Corn.all.filter((c.name == 'foo' ) &
+    items = list(Corn.all.filter((c.name == 'foo') &
         (c.lastname == 'bar')).execute())
     assert len(items) == 1
     assert items[0]['id'] == 1
-    items = list(Corn.all.filter((c.name == 'baz' ) |
+    items = list(Corn.all.filter((c.name == 'baz') |
         (c.lastname == 'baz')).execute())
     assert len(items) == 2
     assert 2 in (x['id'] for x in items)
@@ -74,12 +78,12 @@ def test_optimization(Corn):
     items = list(Corn.all.filter(c.name == 'foo').execute())
     assert len(items) == 2
     assert all(item['name'] == 'foo' for item in items)
-    items = list(Corn.all.filter((c.name == 'foo' ) &
+    items = list(Corn.all.filter((c.name == 'foo') &
         (c.lastname == 'bar')).execute())
     assert len(items) == 1
     assert items[0]['id'] == 1
     assert all([item.corn == Corn for item in items])
-    items = list(Corn.all.filter((c.name == 'baz' ) |
+    items = list(Corn.all.filter((c.name == 'baz') |
         (c.lastname == 'baz')).execute())
     assert len(items) == 2
     assert 2 in (x['id'] for x in items)
@@ -115,24 +119,27 @@ def test_optimization(Corn):
     assert len(items) == 3
     assert all(type(item) == dict for item in items)
     assert all('foo' in item for item in items)
-    items = list(Corn.all.map({'foo': c.name}).filter(c.foo == 'baz').execute())
+    items = list(Corn.all.map({'foo': c.name})
+                 .filter(c.foo == 'baz').execute())
     assert len(items) == 1
     assert all(type(item) == dict for item in items)
     assert all(item['foo'] == 'baz' for item in items)
-    items = list(Corn.all.map(c + {'doubleid' : c.id}).execute())
+    items = list(Corn.all.map(c + {'doubleid': c.id}).execute())
     assert len(items) == 3
     assert all(type(item) == dict for item in items)
     assert all(item['doubleid'] == item['id'] for item in items)
-    items = list(Corn.all.map(c + {'square' : c.id * c.id}).execute())
+    items = list(Corn.all.map(c + {'square': c.id * c.id}).execute())
     assert len(items) == 3
     assert all(type(item) == dict for item in items)
     assert all(item['square'] == item['id'] ** 2 for item in items)
     items = list(Corn.all.map(c + c).execute())
     assert len(items) == 3
     items = list(Corn.all.map(c + Corn.all.filter(c.id == c(-1).id).map({
-            'otherid': c.id, 'othername': c.name, 'otherlastname': c.lastname}).one()).execute())
+            'otherid': c.id, 'othername': c.name, 'otherlastname': c.lastname})
+                              .one()).execute())
     assert all(item['id'] == item['otherid'] for item in items)
-    req = Corn.all.map(c + {'foreign': Corn.all.filter(c.id == c(-1).id).one()})
+    req = Corn.all.map(c + {'foreign': Corn.all.filter(c.id == c(-1).id)
+                            .one()})
     items = list(req.execute())
     assert len(items) == 3
     assert all(hasattr(item['foreign'], 'corn') for item in items)
@@ -147,7 +154,8 @@ def test_optimization(Corn):
     assert items[2]['name'] == 'baz' and items[2]['id'] == 2
     length = Corn.all.len().execute()
     assert length == 3
-    items = list(Corn.all.groupby(c.name, group=c.len()).sort(c.group).execute())
+    items = list(Corn.all.groupby(c.name, group=c.len()).sort(c.group)
+                 .execute())
     assert len(items) == 2
     assert items[0]['group'] == 1
     assert items[1]['group'] == 2
@@ -168,7 +176,8 @@ def test_optimization(Corn):
     assert item == default
     items = list(Corn.all.filter(c.name.upper() == 'FOO').execute())
     assert len(items) == 2
-    items = list(Corn.all.groupby(c.name, group=c.sum(c.id)).sort(c.group).execute())
+    items = list(Corn.all.groupby(c.name, group=c.sum(c.id)).sort(c.group)
+                 .execute())
     assert len(items) == 2
     assert items[0]['key'] == 'baz' and items[0]['group'] == 2
     assert items[1]['key'] == 'foo' and items[1]['group'] == 4
@@ -177,8 +186,11 @@ def test_optimization(Corn):
         'min': c.min(c.id),
         'sum': c.sum(c.id)}).sort(c.key).execute())
     assert len(items) == 2
-    assert items[0]['key'] == 'baz' and items[0]['group'] == {'max': 2, 'min': 2, 'sum': 2}
-    assert items[1]['key'] == 'foo' and items[1]['group'] == {'max': 3, 'min': 1, 'sum': 4}
+    assert items[0]['key'] == 'baz' and items[0]['group'] == {
+
+        'max': 2, 'min': 2, 'sum': 2}
+    assert items[1]['key'] == 'foo' and items[1]['group'] == {
+        'max': 3, 'min': 1, 'sum': 4}
     items = list(Corn.all.map(c.name + ' ' + c.lastname).execute())
     assert len(items) == 3
     items = list(Corn.all.map(c.name.upper()).sort(c).execute())
@@ -191,11 +203,13 @@ def test_optimization(Corn):
     assert len(items) == 2
     assert items[0]['id'] == 1
     assert items[1]['id'] == 2
-    items = list(Corn.all.map(c + {'homonymes': Corn.all.filter(c.name == c(-1).name)}).execute())
+    items = list(Corn.all.map(c + {
+        'homonymes': Corn.all.filter(c.name == c(-1).name)}).execute())
     assert len(items) == 3
     assert all(all(subitem['name'] == item['name']
         for subitem in item['homonymes'])
             for item in items)
+
 
 @suite.test
 @postgres_suite.test
@@ -209,7 +223,8 @@ def test_unicode(Corn):
     Corn.create({'id': 1, 'name': u'foo', 'lastname': u'baré'}).save()
     Corn.create({'id': 2, 'name': u'baz', 'lastname': u'barà'}).save()
     Corn.create({'id': 3, 'name': u'foo', 'lastname': u'baz'}).save()
-    items = list(Corn.all.map(c + {'homonymes': Corn.all.filter(c.name == c(-1).name)}).execute())
+    items = list(Corn.all.map(c + {
+        'homonymes': Corn.all.filter(c.name == c(-1).name)}).execute())
     assert len(items) == 3
     assert all(all(subitem['name'] == item['name']
         for subitem in item['homonymes'])
