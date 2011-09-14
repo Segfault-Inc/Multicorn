f2857a3246c23b18c05085cf3330e705fec0b5a6
Florian Mounier <florian.mounier@kozea.fr>
2011-09-14 12:04:46 +0200

    Seqfault

--- py_fdw/__init__.py
+++
@@ -1,2 +0,0 @@
-def get():
-    return "1337"
---
+++ python/fdw/__init__.py
@@ -0,0 +1,13 @@
+def prnt(options_dict):
+    print options_dict
+    return 1
+
+
+class ForeignDataWrapper(object):
+
+    def __init__(self, fdw_options):
+        pass
+
+    def execute(self, options):
+        # yield element
+        pass
