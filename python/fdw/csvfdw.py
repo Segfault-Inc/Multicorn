945e38b265a9e6cb4930dbfa52485a946717d234
Florian Mounier <florian.mounier@kozea.fr>
2011-09-14 19:02:37 +0200

    Csv Delimiters

--- python/fdw/csvfdw.py
+++ python/fdw/csvfdw.py
@@ -8,15 +8,12 @@ class CsvFdw(ForeignDataWrapper):
         super(CsvFdw, self).__init__(fdw_options)
         print "Csv: %r" % fdw_options
         self.filename = fdw_options["filename"]
-        # self.filename = fdw_options["filename"]
+        self.delimiter = fdw_options["delimiter"]
 
     def execute(self):
         import csv
         with open(self.filename) as fd:
-            reader = csv.reader(fd)
+            reader = csv.reader(fd, delimiter=self.delimiter)
             for line in reader:
                 print line
                 yield line
-        # with open(self.filename) as csv:
-            # for line in csv.readLine():
-                # yield line
--- recreate_extension.sql
+++ recreate_extension.sql
@@ -1,8 +1,8 @@
 drop extension dummy_fdw cascade;
 create extension dummy_fdw;
 create server dummy foreign data wrapper dummy_fdw;
-create foreign table test (field1 numeric, field2 character varying, field3 date) 
-server dummy 
-options (wrapper 'fdw.csvfdw.CsvFdw', filename '/tmp/test.csv', format 'csv', delimiter ';');
+create foreign table test (field1 numeric, field2 character varying, field3 date)
+server dummy
+options (wrapper 'fdw.csvfdw.CsvFdw', filename '/tmp/test.csv', format 'csv', delimiter ',');
 select * from test
 
