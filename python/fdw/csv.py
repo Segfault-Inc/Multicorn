823f241b0b80c8f02ad154ab1dbbc68c43dd7dfa
Florian Mounier <florian.mounier@kozea.fr>
2011-09-14 14:23:52 +0200

    With yield

--- python/fdw/csv.py
+++ python/fdw/csv.py
@@ -5,11 +5,12 @@ class CsvFdw(ForeignDataWrapper):
 
     def __init__(self, fdw_options):
         super(CsvFdw, self).__init__(fdw_options)
-        print "Csv"
+        print "Csv: %r" % fdw_options
         # self.filename = fdw_options["filename"]
 
     def execute(self):
-        return ["1337", "42"]
+        for i in (1337, 42, 1, -23, 0.235):
+            yield str(i)
         # with open(self.filename) as csv:
             # for line in csv.readLine():
                 # yield line
