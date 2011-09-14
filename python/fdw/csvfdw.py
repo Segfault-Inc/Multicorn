005950234ee044519447e3a2a20a4f43e89755d7
Ronan Dunklau <ronan.dunklau@kozea.fr>
2011-09-14 18:45:46 +0200

    Csv works

--- python/fdw/csvfdw.py
+++ python/fdw/csvfdw.py
@@ -7,12 +7,16 @@ class CsvFdw(ForeignDataWrapper):
     def __init__(self, fdw_options):
         super(CsvFdw, self).__init__(fdw_options)
         print "Csv: %r" % fdw_options
+        self.filename = fdw_options["filename"]
         # self.filename = fdw_options["filename"]
 
     def execute(self):
-        for i in (1337, 42, 1, -23, 0.235):
-            yield [i, 'line %i' % i, date.today()]
-            yield {'field1': i, 'field2': 'grou%d' % i, 'field3': date.today()}
+        import csv
+        with open(self.filename) as fd:
+            reader = csv.reader(fd)
+            for line in reader:
+                print line
+                yield line
         # with open(self.filename) as csv:
             # for line in csv.readLine():
                 # yield line
--- recreate_extension.sql
+++ recreate_extension.sql
@@ -3,6 +3,6 @@ create extension dummy_fdw;
 create server dummy foreign data wrapper dummy_fdw;
 create foreign table test (field1 numeric, field2 character varying, field3 date) 
 server dummy 
-options (wrapper 'fdw.csv.CsvFdw', filename '/tmp/statistical_data.csv', format 'csv', delimiter ';');
+options (wrapper 'fdw.csvfdw.CsvFdw', filename '/tmp/test.csv', format 'csv', delimiter ';');
 select * from test
 
