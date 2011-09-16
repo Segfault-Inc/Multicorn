1a3a01c91d1c8d5b7153e88326b6f42f68013bcc
Ronan Dunklau <ronan.dunklau@kozea.fr>
2011-09-16 15:54:49 +0200

    Restore tests

--- recreate_extension.sql
+++ recreate_extension.sql
@@ -1,52 +1,52 @@
--- drop extension multicorn cascade;
--- create extension multicorn;
--- create server multicorn_srv foreign data wrapper multicorn;
--- create foreign table csvtest (
---        field1 numeric,
---        field2 character varying,
---        field3 date
--- ) server multicorn_srv options (
---        wrapper 'multicorn.csvfdw.CsvFdw',
---        filename '/tmp/test.csv',
---        format 'csv',
---        delimiter ',');
--- select * from csvtest;
--- select * from csvtest where field1 = 1;
--- select * from csvtest where field2 = 'test';
+drop extension multicorn cascade;
+create extension multicorn;
+create server multicorn_srv foreign data wrapper multicorn;
+create foreign table csvtest (
+       field1 numeric,
+       field2 character varying,
+       field3 date
+) server multicorn_srv options (
+       wrapper 'multicorn.csvfdw.CsvFdw',
+       filename '/tmp/test.csv',
+       format 'csv',
+       delimiter ',');
+select * from csvtest;
+select * from csvtest where field1 = 1;
+select * from csvtest where field2 = 'test';
 
--- drop extension multicorn cascade;
--- create extension multicorn;
--- create server multicorn_srv foreign data wrapper multicorn;
--- create foreign table ldaptest (
---        cn character varying,
---        sn character varying,
---        uid character varying,
---        "uidNumber" int,
---        o character varying,
---        mail character varying
--- ) server multicorn_srv options (
---        wrapper 'multicorn.ldapfdw.LdapFdw',
---        address 'ldap.keleos.fr',
---        "path" 'ou=People,dc=keleos,dc=fr',
---        objectclass 'inetOrgPerson');
--- select * from ldaptest;
--- select * from ldaptest where uid = 'gayoub';
--- select * from ldaptest where "uidNumber" = 1022;
--- select * from ldaptest where cn like '%y%';
+drop extension multicorn cascade;
+create extension multicorn;
+create server multicorn_srv foreign data wrapper multicorn;
+create foreign table ldaptest (
+       cn character varying,
+       sn character varying,
+       uid character varying,
+       "uidNumber" int,
+       o character varying,
+       mail character varying
+) server multicorn_srv options (
+       wrapper 'multicorn.ldapfdw.LdapFdw',
+       address 'ldap.keleos.fr',
+       "path" 'ou=People,dc=keleos,dc=fr',
+       objectclass 'inetOrgPerson');
+select * from ldaptest;
+select * from ldaptest where uid = 'gayoub';
+select * from ldaptest where "uidNumber" = 1022;
+select * from ldaptest where cn like '%y%';
 
--- drop extension multicorn cascade;
--- create extension multicorn;
--- create server multicorn_srv foreign data wrapper multicorn;
--- create foreign table fstest (
---        field1 character varying collate "fr_FR.utf8",
---        field2 character varying collate "fr_FR.utf8",
---        field3 character varying collate "fr_FR.utf8"
--- ) server multicorn_srv options (
---        wrapper 'multicorn.fsfdw.FilesystemFdw',
---        root_dir '/tmp/data',
---        pattern '{field1}/{field2}/{field3}/style.css');
--- select * from fstest;
--- select * from fstest where field2 = 'test';
+drop extension multicorn cascade;
+create extension multicorn;
+create server multicorn_srv foreign data wrapper multicorn;
+create foreign table fstest (
+       field1 character varying collate "fr_FR.utf8",
+       field2 character varying collate "fr_FR.utf8",
+       field3 character varying collate "fr_FR.utf8"
+) server multicorn_srv options (
+       wrapper 'multicorn.fsfdw.FilesystemFdw',
+       root_dir '/tmp/data',
+       pattern '{field1}/{field2}/{field3}/style.css');
+select * from fstest;
+select * from fstest where field2 = 'test';
 
 
 drop extension multicorn cascade;
