fb64b5f24f508ff70bcf7e63a24d0e5bedf6cde8
Ronan Dunklau <ronan.dunklau@kozea.fr>
2011-09-16 15:39:58 +0200

    Fixed things

--- python/multicorn/fsfdw.py
+++ python/multicorn/fsfdw.py
@@ -8,7 +8,17 @@ class FilesystemFdw(ForeignDataWrapper):
         super(FilesystemFdw, self).__init__(options)
         root_dir = options.get('root_dir')
         pattern = options.get('pattern')
-        self.sd = StructuredDirectory(root_dir, pattern)
+        self.content_property = options.get('content_property', None)
+        self.filename_property = options.get('filename_property', None)
+        self.sd = StructuredDirectory(root_dir, pattern);
 
     def execute(self, quals):
-        return self.sd.get_items()
+        cond = dict((qual.field_name, qual.value) for
+                qual in quals if qual.operator == '=')
+        for item in self.sd.get_items(**cond):
+            new_item = dict(item)
+            if self.content_property:
+                new_item[self.content_property] = item.read()
+            if self.filename_property:
+                new_item[self.filename_property] = item.filename
+            yield new_item
