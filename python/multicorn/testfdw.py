e523db5e8de4aca4eda341b41faf2758ca1ac1b9
Ronan Dunklau <ronan.dunklau@kozea.fr>
2011-10-25 15:12:16 +0200

    Added more tests, removed recreate extension

--- python/multicorn/testfdw.py
+++ python/multicorn/testfdw.py
@@ -1,14 +1,21 @@
 from multicorn import ForeignDataWrapper
+from .utils import log_to_postgres
+from itertools import cycle
 
 class TestForeignDataWrapper(ForeignDataWrapper):
 
     def __init__(self, options, columns):
         super(TestForeignDataWrapper, self).__init__(options, columns)
         self.columns = columns
+        log_to_postgres(str(options))
+        log_to_postgres(str(columns))
 
     def execute(self, quals, columns):
+        log_to_postgres(str(quals))
+        log_to_postgres(str(columns))
+        random_thing = cycle([1, 2, 3])
         for index in range(20):
             line = {}
             for column_name in self.columns:
-                line[column_name] = '%s %s' % (column_name, index)
+                line[column_name] = '%s %s %s' % (column_name, next(random_thing), index)
             yield line
--- test/sql/multicorn_regression_test.sql
+++ test/sql/multicorn_regression_test.sql
@@ -1,11 +1,28 @@
 CREATE EXTENSION multicorn;
-CREATE server multicorn_srv foreign data wrapper multicorn;
+CREATE server multicorn_srv foreign data wrapper multicorn options (
+    wrapper 'multicorn.testfdw.TestForeignDataWrapper'
+);
+CREATE user mapping for postgres server multicorn_srv options (usermapping 'test');
 
 CREATE foreign table testmulticorn (
     test1 character varying,
     test2 character varying
 ) server multicorn_srv options (
-    wrapper 'multicorn.testfdw.TestForeignDataWrapper'
+    option1 'option1'
 );
 
+-- Test "normal" usage
 select * from testmulticorn;
+
+-- Test quals
+select * from testmulticorn where test1 like '%0';
+
+-- Test columns
+select test2 from testmulticorn;
+
+-- Test subquery plan
+select test1, (select max(substr(test1, 9, 1))::int from testmulticorn t2 where substr(t2.test1, 7, 1)::int = substr(t1.test1, 7, 1)::int)
+from testmulticorn t1;
+
+select test1, (select max(substr(test1, 9, 1))::int from testmulticorn t2 where t2.test1 = t1.test1)
+from testmulticorn t1;
