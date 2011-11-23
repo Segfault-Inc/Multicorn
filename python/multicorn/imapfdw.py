544fa85ca97819428de37f9bb41e874795214da3
Ronan Dunklau <ronan.dunklau@kozea.fr>
2011-11-23 09:41:26 +0100

    Or filters in IMAP

--- python/multicorn/imapfdw.py
+++ python/multicorn/imapfdw.py
@@ -2,6 +2,8 @@ from . import ForeignDataWrapper, ANY, ALL
 from .utils import log_to_postgres, ERROR
 import time
 
+from email.header import decode_header
+
 from imapclient import IMAPClient
 
 STANDARD_FLAGS = {
@@ -11,6 +13,11 @@ STANDARD_FLAGS = {
         'draft': 'Draft',
         'recent': 'Recent'
 }
+
+
+def make_or(values):
+    """Create an imap OR filter based on a list of conditions to be or'ed"""
+    return reduce(lambda x, y: '(OR %s %s)' % (x, y), values)
 
 
 class ImapFdw(ForeignDataWrapper):
@@ -41,7 +48,8 @@ class ImapFdw(ForeignDataWrapper):
         if operator not in ('~~', '!~~', '=', '!=', '@>', '&&', '~~*', '!~~*'):
             # Do not manage special operators
             return None
-        if operator in ('~~', '!~~', '~~*', '!~~*') and isinstance(value, basestring):
+        if operator in ('~~', '!~~', '~~*', '!~~*') and\
+                isinstance(value, basestring):
             # 'Normalize' the sql like wildcards
             if value.startswith(('%', '_')):
                 value = value[1:]
@@ -68,9 +76,10 @@ class ImapFdw(ForeignDataWrapper):
                     % ('KEYWORD', atom))))  for atom in value])
             elif operator == '&&':
                 # Overlaps on flags => Or
-                return '(OR %s) ' % ' '.join(['(%s%s)' %
+                values = ['(%s%s)' %
                     (prefix, (STANDARD_FLAGS.get(atom.lower(), '%s %s' %
-                    ('KEYWORD', atom))))  for atom in value])
+                    ('KEYWORD', atom))))  for atom in value]
+                return make_or(values)
             else:
                 value = '\\\\%s' % value
         elif key == self.payload_column:
@@ -85,10 +94,11 @@ class ImapFdw(ForeignDataWrapper):
         for qual in quals:
             # Its a list, so we must translate ANY to OR, and ALL to AND
             if qual.list_any_or_all == ANY:
-                conditions.append('(OR %s)' % ' '.join([
+                values = [
                     '(%s)' % self._make_condition(qual.field_name,
                         qual.operator[0], value)
-                    for value in qual.value]))
+                    for value in qual.value]
+                return make_or(values)
             elif qual.list_any_or_all == ALL:
                 conditions.extend([
                     self._make_condition(qual.field_name, qual.operator[0],
@@ -127,7 +137,13 @@ class ImapFdw(ForeignDataWrapper):
                 for column, key in col_to_imap.iteritems():
                     item[column] = msg[key]
                     if column in headers:
-                        # Values are of the from "Header: value"
                         item[column] = item[column].replace('%s:' %
-                            column.title(), '', 1)
+                            column.title(), '', 1).strip()
+                        values = decode_header(item[column])
+                        for decoded_header, charset in values:
+                            # Values are of the from "Header: value"
+                            if charset:
+                                item[column] = decoded_header.decode(charset)
+                            else:
+                                item[column] = decoded_header
                 yield item
