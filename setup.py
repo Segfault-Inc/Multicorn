22cdc4f83e865a7789e539c4d1d3a66388107667
Ronan Dunklau <ronan.dunklau@kozea.fr>
2012-09-28 14:22:03 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ multicorn_utils_module = Extension('multicorn._utils',
 
 setup(
  name='multicorn',
- version='0.9.2',
+ version='0.9.2.1',
  author='Kozea',
  license='Postgresql',
  package_dir={'': 'python'},
