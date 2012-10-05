d7593659047043e861596ea1ac0aa9b500a6a0fe
Ronan Dunklau <ronan.dunklau@kozea.fr>
2012-10-05 15:49:35 +0200

    Add tests for returning sequences

--- python/multicorn/testfdw.py
+++ python/multicorn/testfdw.py
@@ -12,30 +12,38 @@ class TestForeignDataWrapper(ForeignDataWrapper):
         self.test_type = options.get('test_type', None)
         log_to_postgres(str(options))
         log_to_postgres(str(dict([(key, column.type_name) for key, column in
-            columns.items()])))
+                                  columns.items()])))
 
     def execute(self, quals, columns):
         log_to_postgres(str(quals))
         log_to_postgres(str(columns))
         random_thing = cycle([1, 2, 3])
         for index in range(20):
-            line = {}
-            for column_name in self.columns:
-                if self.test_type == 'list':
-                    line[column_name] = [column_name, next(random_thing),
-                            index]
-                elif self.test_type == 'dict':
-                    line[column_name] = {"column_name": column_name,
-                            "repeater": next(random_thing),
-                            "index": index}
-                elif self.test_type == 'date':
-                    line[column_name] = datetime(2011, (index % 12) + 1,
-                            next(random_thing), 14, 30, 25)
-                else:
-                    line[column_name] = '%s %s %s' % (column_name,
-                            next(random_thing), index)
-            yield line
+            if self.test_type == 'sequence':
+                line = []
+                for column_name in self.columns:
+                    line.append('%s %s %s' % (column_name,
+                                              next(random_thing), index))
+            else:
+                line = {}
+                for column_name in self.columns:
+                    if self.test_type == 'list':
+                        line[column_name] = [column_name, next(random_thing),
+                                             index]
+                    elif self.test_type == 'dict':
+                        line[column_name] = {"column_name": column_name,
+                                             "repeater": next(random_thing),
+                                             "index": index}
+                    elif self.test_type == 'date':
+                        line[column_name] = datetime(2011, (index % 12) + 1,
+                                                     next(random_thing), 14,
+                                                     30, 25)
 
+                    else:
+                        line[column_name] = '%s %s %s' % (column_name,
+                                                          next(random_thing),
+                                                          index)
+            yield line
 
     def get_rel_size(self, quals, columns):
         if self.test_type == 'planner':
