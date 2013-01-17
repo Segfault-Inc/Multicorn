6cf60f1050f8419f63d586c2d4a30f8793e1b20c
Daniël van Eeden <git@myname.nl>
2013-01-17 17:00:35 +0100

    add check for python2.7

--- preflight-check.sh
+++ preflight-check.sh
@@ -2,6 +2,7 @@
 
 PG_CONFIG=$(which pg_config)
 PY_VERSION=$(python --version 2>&1 | awk '{ print substr($2,1,3)}')
+PY27_VERSION=$(python2.7 --version 2>&1 | awk '{ print substr($2,1,3)}')
 
 if [ -z "${PG_CONFIG}" ]; then
   echo "No pg_config found in your path."
@@ -16,6 +17,8 @@ if [ ! -x "${PG_CONFIG}" ]; then
 fi
 
 if [ ${PY_VERSION} != "2.7" ]; then
-  echo "Found Python $PY_VERSION, but 2.7 is required."
-  exit 2
+  if [ ${PY27_VERSION} != "2.7" ]; then
+    echo "Found Python $PY_VERSION, but 2.7 is required."
+    exit 2
+  fi
 fi
