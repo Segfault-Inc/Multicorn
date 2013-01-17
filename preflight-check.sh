86fec6c75d59df632d7515edde4e761ae31fa5df
Daniël van Eeden <git@myname.nl>
2013-01-17 16:15:06 +0100

    test on empty string

--- preflight-check.sh
+++ preflight-check.sh
@@ -3,7 +3,13 @@
 PG_CONFIG=$(which pg_config)
 PY_VERSION=$(python --version 2>&1 | awk '{ print substr($2,1,3)}')
 
-if [ -x ${PG_CONFIG} ]; then
+if [ -z "${PG_CONFIG}" ]; then
+  echo "No pg_config found in your path."
+  echo "Please check if you installed the PostgreSQL development packages."
+  exit 1
+fi
+
+if [ ! -x "${PG_CONFIG}" ]; then
   echo "No pg_config found in your path."
   echo "Please check if you installed the PostgreSQL development packages."
   exit 1
