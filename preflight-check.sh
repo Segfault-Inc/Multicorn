29ae005f22c7bc8cd504a177cc5957a5e0f27e32
Daniël van Eeden <git@myname.nl>
2013-01-17 16:04:19 +0100

    Add preflight check.

---
+++ preflight-check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PG_CONFIG=$(which pg_config)
+PY_VERSION=$(python --version 2>&1 | awk '{ print substr($2,1,3)}')
+
+if [ -x ${PG_CONFIG} ]; then
+  echo "No pg_config found in your path."
+  echo "Please check if you installed the PostgreSQL development packages."
+  exit 1
+fi
+
+if [ ${PY_VERSION} != "2.7" ]; then
+  echo "Found Python $PY_VERSION, but 2.7 is required."
+  exit 2
+fi
