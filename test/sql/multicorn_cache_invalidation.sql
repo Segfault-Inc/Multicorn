8297ea3e6b7c6b06b0beb4354d586345492c90e0
Ronan Dunklau <ronan.dunklau@kozea.fr>
2013-04-05 08:19:47 +0200

    Add a python instance cache invalidation mechanism.
    Before this, a cached FDW instance would be kept until the death of the backend.
    This patch caches a list of options and a list of column definitions to be
    compared against the current set of columns / options of the foreign table. If
    they don't match, the cached instance will be replaced by a fresh instance using
    the new values. Fix #11.

---
+++ test/sql/multicorn_cache_invalidation.sql
@@ -0,0 +1,41 @@
+CREATE EXTENSION multicorn;
+CREATE server multicorn_srv foreign data wrapper multicorn options (
+    wrapper 'multicorn.testfdw.TestForeignDataWrapper'
+);
+CREATE user mapping for postgres server multicorn_srv options (usermapping 'test');
+
+CREATE foreign table testmulticorn (
+    test1 character varying,
+    test2 character varying
+) server multicorn_srv options (
+    option1 'option1'
+);
+
+-- Test "normal" usage
+select * from testmulticorn;
+
+ALTER foreign table testmulticorn drop column test1;
+
+select * from testmulticorn;
+
+ALTER foreign table testmulticorn add column test1 varchar;
+
+select * from testmulticorn;
+
+ALTER foreign table testmulticorn add column test3 varchar;
+
+select * from testmulticorn;
+
+ALTER foreign table testmulticorn options (SET option1 'option1_update');
+
+select * from testmulticorn;
+
+ALTER foreign table testmulticorn options (ADD option2 'option2');
+
+select * from testmulticorn;
+
+ALTER foreign table testmulticorn options (DROP option2);
+
+select * from testmulticorn;
+
+DROP extension multicorn cascade;
