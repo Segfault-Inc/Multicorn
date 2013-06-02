a3b53faae161cc391ec41d5ae38064ae8f96955e
Reece Hart <reecehart@gmail.com>
2013-06-01 20:57:00 -0700

    syapsefdw: use coldefs for savedqueries

--- python/multicorn/syapsefdw.py
+++ python/multicorn/syapsefdw.py
@@ -7,28 +7,43 @@ import syapse_client
 
 ############################################################################
 
+# TODO: column types (rule-based?); date_, has_->aii (array), 
+# TODO: column name mapping
+# TODO: rule-based column name mapping (has_, cardinality -> +s, etc)
+# TODO: explicit prop -> col mapping (instead of relying on zip order)
+# TODO: has_ -> list, always
+
+# TODO: reconnect syapse on failure?
+# TODO: config-based setup
+
+############################################################################
+
+class SyapseFDWFactory(object):
+    """This class creates new subclasses of SyapseFDW based on the
+    provided arguments to the constructor.
+
+    Unlike traditional factories, this factory creates new instances of
+    other classes through its constructor rather than through a dedicated
+    method.  You will never get an instance of SyapseFDWFactory itself.
+
+    Why?  Multicorn setup requires calling a class constructor for the
+    FDW.  When multiple subclasses are used to handle FDWs for a single
+    server, it's convenient to be able to instantiate subclasses based on
+    arguments."""
+    def __new__(cls,options,columns):
+        return create_fdw(options,columns)
+    def __init__(self):
+        pass
+
 def create_fdw(options,columns):
     """simple factory to make different FDW depending on source"""
     cl = ClassTable
     if options.get('source').startswith('fdw:'):
         cl = SavedQueryTable
     fdw = cl(options,columns)
-    log_to_postgres(message = 'created '+str(type(fdw)))
+    log_to_postgres(message = 'created '+str(type(fdw))+' for '+options.get('source'))
     return fdw
 
-
-class SyapseFDWFactory(object):
-    """Attempting to create an instance of this class actually returns
-    objects of another class that is chosen based on the provided
-    arguments.  Unlike traditional factories, this factory creates new
-    instances of other classes through its constructor rather than through
-    a dedicated method.  This class exists because multicorn knows only
-    how to call classes (rather than, say, factory functions), but it's
-    convenient to be able to instantiate subclasses based on arguments."""
-    def __new__(cls,options,columns):
-        return create_fdw(options,columns)
-    def __init__(self):
-        pass
 
 ############################################################################
 
@@ -49,6 +64,21 @@ class SyapseFDW(ForeignDataWrapper):
     def execute(self, quals, columns):
         raise NotImplemented('execute method must be subclassed')
 
+    def table_ddl(self):
+        cols = [ '{cd.column:30} {cd.type:10} -- {cd.syapse_cardinality:10}; {cd.syapse_type:10}; {cd.syapse_property}'.format(cd=cd)
+                 for cd in self.coldefs ]
+        return """DROP FOREIGN TABLE {tablename};
+CREATE FOREIGN TABLE {tablename} (
+    {cols}
+) server syapse options (
+  syapse_hostname '{self.options[syapse_hostname]}',
+  syapse_email    '{self.options[syapse_email]}',
+  syapse_password '{self.options[syapse_password]}',
+  source      	  '{self.options[source]}'
+);
+""".format( tablename = self.tablename,
+            cols = "\n  , ".join(cols),
+            self=self )
 
 class ClassTable(SyapseFDW):
     """Represents a Syapse class-based data source.  The current Syapse
@@ -59,101 +89,91 @@ class ClassTable(SyapseFDW):
     def __init__(self,options,columns):
         super(ClassTable, self).__init__(options,columns)
         self.syapse_class = options['source']
-        self._build_coldef_table()
+        self.tablename = camelcase_to_underscore(self.syapse_class)
+        self._build_coldefs()
 
     def execute(self, quals, columns):
         airs = self.conn.kb.listAppIndividualRecords(kb_class_id=self.syapse_class)
         for air in airs:
             ai = self.conn.kb.retrieveAppIndividual(air.app_ind_id)
             triples_items = ai.triplesItems(full=True)
+            # BROKEN HERE: adapt to coldefs list rather than coldefs dict
             row = dict([ (self.coldefs[k].column, _transform_value(self.coldefs[k],_map_values(vals)))
                          for k,vals in triples_items ])
             yield row
 
-    def _build_coldef_table(self):
+    def _build_coldefs(self):
         """builds a dict of syapse property name => ColDef record"""
-
-        def __syapse_type(ps):
+        def _syapse_type(ps):
             return ps.prop.range if isinstance(ps.prop.range,unicode) else None
-
-        def _make_coldef_record(ps):
+        def _make_coldef(ps):
             return ColDef(syapse_property = ps.prop.id, 
-                          syapse_type = __syapse_type(ps),
+                          syapse_type = _syapse_type(ps),
                           syapse_cardinality = ps.cardinality,
                           column = camelcase_to_underscore(ps.prop.id),
                           type = 'TEXT',
                         )
-        self.coldefs = dict(
-            (ps.prop.id, _make_coldef_record(ps))
-            for ps in self.conn.kb.getForm(self.syapse_class).props.values()
-            )
-
-    def table_ddl(self):
-        cols = [ '{cd.column:30} {cd.type:10} -- {cd.syapse_cardinality:10}; {cd.syapse_type:10}; {cd.syapse_property}'.format(cd=cd)
-                 for cd in self.coldefs.values() ]
-        return """DROP FOREIGN TABLE {tablename};
-CREATE FOREIGN TABLE {tablename} (
-    {cols}
-) server syapse options (
-  syapse_hostname '{self.options[syapse_hostname]}',
-  syapse_email    '{self.options[syapse_email]}',
-  syapse_password '{self.options[syapse_password]}',
-  source      	  '{self.options[source]}'
-);
-""".format( tablename = camelcase_to_underscore(self.syapse_class),
-            cols = "\n  , ".join(cols),
-            self=self )
+        self.coldefs = [ _make_coldef(ps)
+                         for ps in self.conn.kb.getForm(self.syapse_class).props.values() ]
 
 
 class SavedQueryTable(SyapseFDW):
     def __init__(self,options,columns):
         super(SavedQueryTable, self).__init__(options,columns)
-        sqs = dict( self.conn.getAllSavedQueries() )
+        self.conn._sqs = dict( self.conn.getAllSavedQueries() ) # add to conn object, shared across conns
         self.syapse_saved_query = options['source']
-        self.syapse_saved_query_id = sqs[ self.syapse_saved_query ]
-
-        ds = self.conn.kb.executeSavedQuery( self.syapse_saved_query_id )
-        self.ds_headers = ds.headers
-        self.headers = ['aii']  # app ind id
-        self.headers += [ camelcase_to_underscore(h.split(':')[-1]) 
-                          for h in self.ds_headers ]
+        self.syapse_saved_query_id = self.conn._sqs[ self.syapse_saved_query ]
+        self.tablename = savedquery_to_tablename(self.syapse_saved_query)
+        if 'conf' in options:
+            self.conf = SafeConfigParser()
+            self.conf.read( open(options['conf'],'r') )
+        self._build_coldefs()
 
     def execute(self, quals, columns):
-        def _process_ds_row(ds_row):
+        def _process_ds_row(coldefs,ds_row):
             # horrors. The AII is buried in "AnnotatedValue" records,
             # which are returned for system properties when the saved
             # query is executed with annotate_meta=True.  These AV
             # instances happen to contain the AII for the current
             # record. We fish the AII out of any of these AVs.
-            vals = [ v.value if isinstance(v,syapse_client.sem.advq.AnnotatedValue) else v
-                     for v in ds_row ]
             avs = [ v for v in ds_row 
                     if isinstance(v,syapse_client.sem.advq.AnnotatedValue) ]
             aii = avs[0].app_ind_id if len(avs) > 0 else None
-            row = dict(zip(self.headers,[aii] + vals))
+            vals = [ v.value if isinstance(v,syapse_client.sem.advq.AnnotatedValue) else v
+                     for v in [aii] + ds_row ]
+            row = dict( [ (cd.column, _transform_value(val,cd.syapse_cardinality))
+                           for cd,val in zip(coldefs,vals) ])
+            #if row['workflow_aii'] != u'ivld:LocusWorkflow_19588':
+            #    return None
+            #import IPython; IPython.embed()
             return row
-            
+
         ds = self.conn.kb.executeSavedQuery( self.syapse_saved_query_id,
                                              annotate_meta = True)
         for ds_row in ds.rows:
-            row = _process_ds_row(ds_row)
+            row = _process_ds_row(self.coldefs,ds_row)
             yield row
 
-    def table_ddl(self):
-        cols = [ '{column:30} {type:10}'.format(column = h, type = 'TEXT')
-                 for h in self.headers ]
-        return """DROP FOREIGN TABLE {tablename};
-CREATE FOREIGN TABLE {tablename} (
-    {cols}
-) server syapse options (
-  syapse_hostname '{self.options[syapse_hostname]}',
-  syapse_email    '{self.options[syapse_email]}',
-  syapse_password '{self.options[syapse_password]}',
-  source      	  '{self.options[source]}'
-);
-""".format( tablename = savedquery_to_tablename(self.syapse_saved_query),
-            cols = "\n  , ".join(cols),
-            self=self )
+    def _build_coldefs(self):
+        """builds a list ColDef records"""
+        def _make_coldef(h):
+            col = camelcase_to_underscore(h.split(':')[-1])
+            type = 'TEXT'
+            if col.startswith('date_'):
+                type = 'TIMESTAMP'
+            card = 'ExactlyOne'
+            if col.startswith('has_'):
+                card = 'Any'
+                type += '[]'
+            return ColDef(syapse_property = h,
+                                     syapse_type = None,
+                                     syapse_cardinality = card,
+                                     column = col,
+                                     type = type,
+                                     )
+        ds = self.conn.kb.executeSavedQuery( self.syapse_saved_query_id )
+        self.coldefs = [ _make_coldef(h)
+                         for h in [self.tablename+'_aii'] + ds.headers ]
         
                     
 ############################################################################
@@ -190,6 +210,7 @@ class ConnectionPool(object):
                     hostname = hostname, email = email))
         return self.pool[hep]
 
+connection_pool = ConnectionPool()
 
 ############################################################################
 ## UTILITIES
@@ -204,10 +225,13 @@ def _map_values(vals):
         return v
     return [ _map_value(v) for v in vals ]
 
-def _transform_value(coldef,v):
+def _transform_value(v,cardinality):
+    # v may be a list or scalar, and cardinality may be any
     # if cardinality is 'Any', return set
-    # else cadinality is 'ExactlyOne' or 'AtMostOne', return None or value
-    if coldef.syapse_cardinality == 'Any':
+    if cardinality == 'Any':
+        return v if isinstance(v,list) else [v]
+    # else cardinality is 'ExactlyOne' or 'AtMostOne', return None or value
+    if not isinstance(v,list):
         return v
     return v[0] if len(v)>0 else None
 
@@ -222,6 +246,4 @@ def savedquery_to_tablename(t):
     return camelcase_to_underscore(t)
 
 ############################################################################
-
-connection_pool = ConnectionPool()
 
