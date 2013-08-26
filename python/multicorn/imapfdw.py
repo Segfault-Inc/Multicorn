711db43bfe67028a7700576b9a162154afe7b727
Florian Mounier <florian.mounier@kozea.fr>
2013-08-26 11:33:25 +0200

    Fix zip_longest in python2

--- python/multicorn/imapfdw.py
+++ python/multicorn/imapfdw.py
@@ -8,7 +8,11 @@ from multicorn.compat import basestring_
 from email.header import decode_header
 
 from imapclient import IMAPClient
-from itertools import zip_longest
+try:
+    from itertools import zip_longest
+except ImportError:
+    from itertools import izip_longest as zip_longest
+
 from functools import reduce
 
 
