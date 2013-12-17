452751fefb7bcc50b4b21c9e58e9fe05c8fcc756
Ronan Dunklau <ronan@dunklau.fr>
2013-12-17 08:31:25 +0100

    Some test cleanup.

--- test-3.3/sql/multicorn_logger_test.sql
+++ test-3.3/sql/multicorn_logger_test.sql
@@ -1,18 +1,1 @@
-SET client_min_messages=NOTICE;
-CREATE EXTENSION multicorn;
-CREATE server multicorn_srv foreign data wrapper multicorn options (
-    wrapper 'multicorn.testfdw.TestForeignDataWrapper'
-);
-
-CREATE foreign table testmulticorn (
-    test1 character varying,
-    test2 character varying
-) server multicorn_srv options (
-    option1 'option1',
-    test_type 'logger'
-);
-
--- Test "normal" usage
-select * from testmulticorn;
-
-DROP EXTENSION multicorn cascade;
+../../test-2.7/sql/multicorn_logger_test.sql
