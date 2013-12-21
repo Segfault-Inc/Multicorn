86d70752e994aa60c8165d16a3a7c25150428561
Christian Toivola <c.toivola@dev360.com>
2013-12-20 19:59:34 -0500

    Adds s3fdw

--- setup.py
+++ setup.py
@@ -9,8 +9,9 @@ setup(
  name='multicorn',
  version='__VERSION__',
  author='Kozea',
+ install_requires=['boto',],
  license='Postgresql',
  package_dir={'': 'python'},
  packages=['multicorn', 'multicorn.fsfdw'],
- ext_modules = [multicorn_utils_module]
+ ext_modules = [multicorn_utils_module],
 )
