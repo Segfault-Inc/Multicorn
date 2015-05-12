39b337a07b59034bdb58c78b1712521380f01f5e
Ben Greene <B.Greene@analyticsengines.com>
2015-05-12 03:55:57 +0100

    Working version of building rpm with multicorn.so. Now need to test it.

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 import subprocess
 from setuptools import setup, find_packages, Extension
 
+
+import os
+from sys import platform
+#from setuptools import setup
+from setuptools.command.install import install
+from distutils.command.build import build
+
+
 # hum... borrowed from psycopg2
 def get_pg_config(kind, pg_config="pg_config"):
     r = subprocess.check_output([pg_config, '--%s' % kind])
@@ -16,13 +24,48 @@ multicorn_utils_module = Extension('multicorn._utils',
         extra_compile_args = ['-shared'],
         sources=['src/utils.c'])
 
+
+class MulticornBuild(build):
+  def run(self):
+    # Original build
+    build.run(self)
+    r = subprocess.check_output(['/usr/bin/make', 'multicorn.so'])
+    r = r.strip().decode('utf8')
+    if not r:
+      raise Warning(p[2].readline())
+    # After original build
+
+class MulticornInstall(install):
+    def initialize_options(self):
+        install.initialize_options(self)
+        self.build_scripts = None
+
+    def finalize_options(self):
+        install.finalize_options(self)
+        self.set_undefined_options('build', ('build_scripts', 'build_scripts'))
+
+    def run(self):
+        # run original install code
+        install.run(self)
+
+        # install XCSoar executables
+        self.copy_tree(self.build_lib, self.install_lib)
+
+
+
 setup(
- name='multicorn',
- version='__VERSION__',
- author='Kozea',
- license='Postgresql',
- package_dir={'': 'python'},
- packages=['multicorn', 'multicorn.fsfdw'],
- ext_modules = [multicorn_utils_module],
- data_files=[('/usr/pgsql-9.3/share/extension', ['multicorn.control', 'sql/multicorn--1.1.1.sql'])],
+  name='multicorn',
+  version='__VERSION__',
+  author='Kozea',
+  license='Postgresql',
+  package_dir={'': 'python'},
+  packages=['multicorn', 'multicorn.fsfdw'],
+  ext_modules = [multicorn_utils_module],
+  data_files=[('%s/extension' % get_pg_config('sharedir'), ['multicorn.control', 'sql/multicorn--1.1.1.sql', 'doc/multicorn.md']),
+	(get_pg_config('libdir'),['multicorn.so'])
+	],
+  cmdclass={
+    'build':MulticornBuild,
+  #  'install':MulticornInstall,
+  }
 )
