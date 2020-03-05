c1a5d7944031279b39b463abe83c10463704b8e5
Ross Biro <rbiro@interfacefinancial.com>
2020-03-05 11:21:34 -0500

    Debugging output.
    --------- BEGIN META VARIABLES --------
    Project :
    Task :
    Done :
    --------- END META VARIABLES --------

--- python/multicorn/utils.py
+++ python/multicorn/utils.py
@@ -172,7 +172,8 @@ def execute_stmt(plan, args, converters):
 
     if not isinstance(converters, tuple):
         converters = tuple (c for c in converters );
-        
+    import logging
+    logging.debug("execute_stmt: about to call _execute_stmt")
     return _execute_stmt(plan, args, converters)
 
 def fetch():
